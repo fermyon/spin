5e64c1cb1fd69dabb8bbd0da11ef364b8c8d0360
Radu Matei <radu.matei@fermyon.com>
2022-02-24 09:57:30 +0200

    feat: allow passing environment variables from CLI
    This commit enables adding environment variables through CLI options
    to all components of an application when starting with `spin up`.
    
    This implementation mutates the component configuration, and would have
    to be replicated for all future triggers.
    A more generic approach might make more sense (the `prepare_component`
    function in the execution context allows taking an optional map of
    environment variables, but this is the smallest change that gives us the
    ability to override environment variables on the CLI).
    
    Signed-off-by: Radu Matei <radu.matei@fermyon.com>

--- src/commands/up.rs
+++ src/commands/up.rs
@@ -1,4 +1,5 @@
 use anyhow::{bail, Result};
+use spin_config::{Configuration, CoreComponent};
 use spin_http_engine::HttpTrigger;
 use std::path::PathBuf;
 use structopt::{clap::AppSettings, StructOpt};
@@ -14,20 +15,19 @@ const BINDLE_URL_ENV: &str = "BINDLE_URL";
     about = "Start the default HTTP listener",
     global_settings = &[AppSettings::ColoredHelp, AppSettings::ArgRequiredElseHelp]
 )]
-pub struct Up {
+
+pub struct UpCommand {
     /// IP address and port to listen on
     #[structopt(long = "listen", default_value = "127.0.0.1:3000")]
     pub address: String,
-
-    /// Path to spin.toml
+    /// Path to spin.toml.
     #[structopt(
         name = APP_CONFIG_FILE_OPT,
         long = "app",
         conflicts_with = BINDLE_ID_OPT,
     )]
     pub app: Option<PathBuf>,
-
-    /// Id of application bindle
+    /// ID of application bindle.
     #[structopt(
         name = BINDLE_ID_OPT,
         long = "bindle",
@@ -35,33 +35,54 @@ pub struct Up {
         requires = BINDLE_SERVER_URL_OPT,
     )]
     pub bindle: Option<String>,
-
-    /// URL of bindle server
+    /// URL of bindle server.
     #[structopt(
         name = BINDLE_SERVER_URL_OPT,
         long = "server",
         env = BINDLE_URL_ENV,
     )]
     pub server: Option<String>,
-
     /// Temorary directory for the static assets of the components.
     #[structopt()]
     pub tmp: Option<PathBuf>,
+    /// Pass an environment variable (key=value) to all components of the application.
+    #[structopt(long = "env", short = "e", parse(try_from_str = parse_env_var))]
+    env: Vec<(String, String)>,
 }
 
-impl Up {
+impl UpCommand {
     pub async fn run(self) -> Result<()> {
-        let app = match (&self.app, &self.bindle) {
+        let mut app = match (&self.app, &self.bindle) {
             (None, None) => bail!("Must specify app file or bindle id"),
             (Some(app), None) => spin_loader::from_file(app, self.tmp).await?,
             (None, Some(bindle)) => match &self.server {
                 Some(server) => spin_loader::from_bindle(bindle, server, self.tmp).await?,
                 _ => bail!("Loading from a bindle requires a Bindle server URL"),
             },
-            (Some(_), Some(_)) => bail!("Specify only one of app file or bindle id"),
+            (Some(_), Some(_)) => bail!("Specify only one of app file or bindle ID"),
         };
+        append_env(&mut app, &self.env)?;
 
         let trigger = HttpTrigger::new(self.address, app, None).await?;
         trigger.run().await
     }
+}
+
+/// Parse the environment variables passed in `key=value` pairs.
+fn parse_env_var(s: &str) -> Result<(String, String)> {
+    let parts: Vec<_> = s.splitn(2, '=').collect();
+    if parts.len() != 2 {
+        bail!("Environment variable must be of the form `key=value`");
+    }
+    Ok((parts[0].to_owned(), parts[1].to_owned()))
+}
+
+/// Append the environment variables passed as options to all components.
+fn append_env(app: &mut Configuration<CoreComponent>, env: &[(String, String)]) -> Result<()> {
+    for c in app.components.iter_mut() {
+        for (k, v) in env {
+            c.wasm.environment.insert(k.clone(), v.clone());
+        }
+    }
+    Ok(())
 }

6b611afcd3ac484955be7f02280923b70189442e
itowlson <ivan.towlson@fermyon.com>
2022-03-23 10:40:40 +1300

    Fix being unable to mount assets from Windows

--- tests/http/simple-spin-rust/src/lib.rs
+++ tests/http/simple-spin-rust/src/lib.rs
@@ -7,11 +7,30 @@ wit_bindgen_rust::export!("spin-http.wit");
 struct SpinHttp {}
 impl spin_http::SpinHttp for SpinHttp {
     // Implement the `handler` entrypoint for Spin HTTP components.
-    fn handle_http_request(_req: Request) -> Response {
-        Response {
-            status: 200,
-            headers: None,
-            body: Some("I'm a teapot".as_bytes().to_vec()),
+    fn handle_http_request(req: Request) -> Response {
+        let path = req.uri;
+
+        if path.contains("test-placement") {
+            match std::fs::read_to_string("/test.txt") {
+                Ok(text) =>
+                    Response {
+                        status: 200,
+                        headers: None,
+                        body: Some(text.as_bytes().to_vec()),
+                    },
+                Err(e) =>
+                    Response {
+                        status: 500,
+                        headers: None,
+                        body: Some(format!("ERROR! {:?}", e).as_bytes().to_vec()),
+                    },
+            }
+        } else {
+            Response {
+                status: 200,
+                headers: None,
+                body: Some("I'm a teapot".as_bytes().to_vec()),
+            }
         }
     }
 }

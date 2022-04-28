55c3f9515c357fff7df761a87942d9cb95112bb8
Vaughn Dice <vaughn.dice@fermyon.com>
2022-04-28 15:34:57 -0600

    add with_context to bindle client generation
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- crates/publish/src/bindle_pusher.rs
+++ crates/publish/src/bindle_pusher.rs
@@ -11,8 +11,14 @@ pub async fn push_all(
     bindle_connection_info: crate::BindleConnectionInfo,
 ) -> Result<()> {
     let reader = StandaloneRead::new(&path, bindle_id).await?;
+    let client = &bindle_connection_info.client().with_context(|| {
+        format!(
+            "Failed to create a bindle client for server '{}'",
+            &bindle_connection_info.base_url
+        )
+    })?;
     reader
-        .push(&bindle_connection_info.client()?)
+        .push(client)
         .await
         .with_context(|| push_failed_msg(path, &bindle_connection_info.base_url))
 }

fa3ab74ebeca3d060793b2ea487448abf1adbde9
Hanzhen Liang <handrenliang@gmail.com>
2022-05-01 22:03:17 +0200

    Remove (Optional) from description doc
    Which already implicit in the Option type.
    
    Signed-off-by: Hanzhen Liang <handrenliang@gmail.com>

--- crates/loader/src/local/config.rs
+++ crates/loader/src/local/config.rs
@@ -61,7 +61,7 @@ pub struct RawComponentManifest {
     /// ID of the component. Used at runtime to select between
     /// multiple components of the same application.
     pub id: String,
-    /// Description of the component(Optional).
+    /// Description of the component.
     pub description: Option<String>,
     /// Per-component WebAssembly configuration.
     #[serde(flatten)]

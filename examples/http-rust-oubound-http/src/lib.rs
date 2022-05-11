09ad9d7931122abcbd21b7081355069bf0418ce5
Jiaxiao Zhou <jiazho@microsoft.com>
2022-05-11 06:11:06 -0700

    feat: Implement a wildcard in http allowed_domains to allow all domains (#422)
    * added wildcard host and a rust outbound http example
    * updated outbound http upstream
    
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

---
+++ examples/http-rust-oubound-http/src/lib.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use spin_sdk::{
+    http::{Request, Response},
+    http_component,
+};
+
+/// Send an HTTP request and return the response.
+#[http_component]
+fn send_outbound(_req: Request) -> Result<Response> {
+    let mut res = spin_sdk::outbound_http::send_request(
+        http::Request::builder()
+            .method("GET")
+            .uri("https://some-random-api.ml/facts/dog")
+            .body(None)?,
+    )?;
+    res.headers_mut()
+        .insert("spin-component", "rust-outbound-http".try_into()?);
+    println!("{:?}", res);
+    Ok(res)
+}

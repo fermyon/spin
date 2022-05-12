2a493e1ac4fcc15404566b003735e46b6e9b339a
itowlson <ivan.towlson@fermyon.com>
2022-05-12 15:49:59 +1200

    Redis Go app template
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

---
+++ templates/redis-go/content/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fermyon/spin/sdk/go/redis"
+)
+
+func init() {
+	// redis.Handle() must be called in the init() function.
+	redis.Handle(func(payload []byte) error {
+		fmt.Println("Payload::::")
+		fmt.Println(string(payload))
+		return nil
+	})
+}
+
+// main functiion must be included for the compiler but is not executed.
+func main() {}

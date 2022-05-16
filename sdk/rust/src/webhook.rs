7c43a3434fd330878e6fffd8b9b1d5a9165c5450
Jiaxiao Zhou <jiazho@microsoft.com>
2022-05-16 15:49:09 -0700

    run cargo fmt
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- sdk/rust/src/webhook.rs
+++ sdk/rust/src/webhook.rs
@@ -24,11 +24,14 @@ pub fn validate_webhook(req: Request, callback_allowed: bool, rate: &str) -> Res
     };
 
     let mut res = http::Response::builder()
-            .status(200)
-            .body(Some("OK".into()))?;
+        .status(200)
+        .body(Some("OK".into()))?;
 
     if callback_allowed {
-        let callback = req.headers().iter().find(|h| h.0 == CALLBACK_REQUEST_HEADER);
+        let callback = req
+            .headers()
+            .iter()
+            .find(|h| h.0 == CALLBACK_REQUEST_HEADER);
         let callback = match callback {
             Some(h) => h.1,
             None => return Err(anyhow::anyhow!("missing webhook-request-callback header")),
@@ -40,12 +43,13 @@ pub fn validate_webhook(req: Request, callback_allowed: bool, rate: &str) -> Res
             .uri(callback.to_str()?)
             .body(None)
             .map_err(|e| anyhow::anyhow!("failed to build request: {}", e))?;
-        res = send_request(req).map_err(|err| {
-            anyhow::anyhow!("failed to send request with error: {:?}", err)
-        })?;
+        res = send_request(req)
+            .map_err(|err| anyhow::anyhow!("failed to send request with error: {:?}", err))?;
     }
-    res.headers_mut().insert(ORIGIN_RESPONSE_HEADER, origin.into());
-    res.headers_mut().insert(RATE_RESPONSE_HEADER, rate.try_into()?);
+    res.headers_mut()
+        .insert(ORIGIN_RESPONSE_HEADER, origin.into());
+    res.headers_mut()
+        .insert(RATE_RESPONSE_HEADER, rate.try_into()?);
     Ok(res)
 }
 
@@ -54,11 +58,17 @@ fn validate_rate(rate: &str) -> Result<&str> {
         match i32::from_str(rate) {
             Ok(rate) => {
                 if rate < 1 {
-                    return Err(anyhow::anyhow!("invalid rate: {}, not a positive number", rate));
+                    return Err(anyhow::anyhow!(
+                        "invalid rate: {}, not a positive number",
+                        rate
+                    ));
                 }
             }
             Err(_) => {
-                return Err(anyhow::anyhow!("invalid rate: {}, not a positive number or *", rate));
+                return Err(anyhow::anyhow!(
+                    "invalid rate: {}, not a positive number or *",
+                    rate
+                ));
             }
         }
     };
@@ -68,23 +78,33 @@ fn validate_rate(rate: &str) -> Result<&str> {
 /// test validate_webhook
 #[cfg(test)]
 mod tests {
-    // use super::*;
-    // use bytes::Bytes;
-    // use http::{Request, Response, HeaderValue};
+    use super::*;
     use anyhow::Result;
+    use bytes::Bytes;
+    use http::{HeaderValue, Request, Response};
 
     #[test]
     fn test_validate_webhook() -> Result<()> {
-        // let req = Request::builder()
-        //     .method(http::Method::OPTIONS)
-        //     .header(ORIGIN_REQUEST_HEADER, "eventemitter.example.com")
-        //     .body(Some(Bytes::from_static(b"")))
-        //     .unwrap();
-        
-        // let res = validate_webhook(req, false, "*")?;
-        // assert_eq!(res.status(), 200);
-        // assert_eq!(res.headers().get(ORIGIN_RESPONSE_HEADER).and_then(|v| Some(v.to_str().unwrap())), Some("eventemitter.example.com"));
-        // assert_eq!(res.headers().get(RATE_RESPONSE_HEADER).and_then(|v| Some(v.to_str().unwrap())), Some("*"));
+        let req = Request::builder()
+            .method(http::Method::OPTIONS)
+            .header(ORIGIN_REQUEST_HEADER, "eventemitter.example.com")
+            .body(Some(Bytes::from_static(b"")))
+            .unwrap();
+
+        let res = validate_webhook(req, false, "*")?;
+        assert_eq!(res.status(), 200);
+        assert_eq!(
+            res.headers()
+                .get(ORIGIN_RESPONSE_HEADER)
+                .and_then(|v| Some(v.to_str().unwrap())),
+            Some("eventemitter.example.com")
+        );
+        assert_eq!(
+            res.headers()
+                .get(RATE_RESPONSE_HEADER)
+                .and_then(|v| Some(v.to_str().unwrap())),
+            Some("*")
+        );
         Ok(())
     }
 }

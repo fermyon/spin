1e46dae316e5a229ebe060de53b8f95c58f6261d
itowlson <ivan.towlson@fermyon.com>
2022-06-10 14:53:05 +1200

    Template version affinity
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>
    
    Name the complicated struct
    
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>
    
    Fixed bad shadowing
    
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>
    
    Use only major and minor semver fields for version tag match
    
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- crates/templates/src/source.rs
+++ crates/templates/src/source.rs
@@ -8,6 +8,7 @@ use url::Url;
 use crate::directory::subdirectories;
 
 const TEMPLATE_SOURCE_DIR: &str = "templates";
+const TEMPLATE_VERSION_TAG_PREFIX: &str = "spin/templates/v";
 
 /// A source from which to install templates.
 #[derive(Debug)]
@@ -18,12 +19,7 @@ pub enum TemplateSource {
     ///
     /// Templates much be in a `/templates` directory under the root of the
     /// repository.
-    Git {
-        /// The URL of the Git repository from which to install templates.
-        url: Url,
-        /// The branch or tag from which to install templates; HEAD if omitted.
-        branch: Option<String>,
-    },
+    Git(GitTemplateSource),
     /// Install from a directory in the file system.
     ///
     /// Templates much be in a `/templates` directory under the specified
@@ -31,17 +27,34 @@ pub enum TemplateSource {
     File(PathBuf),
 }
 
+/// Settings for installing templates from a Git repository.
+#[derive(Debug)]
+pub struct GitTemplateSource {
+    /// The URL of the Git repository from which to install templates.
+    url: Url,
+    /// The branch or tag from which to install templates; inferred if omitted.
+    branch: Option<String>,
+    /// The version of the Spin client, used for branch inference.
+    // We have to pass this through because vergen is only on the root bin
+    spin_version: String,
+}
+
 impl TemplateSource {
     /// Creates a `TemplateSource` referring to the specified Git repository
     /// and branch.
-    pub fn try_from_git(git_url: impl AsRef<str>, branch: &Option<String>) -> anyhow::Result<Self> {
+    pub fn try_from_git(
+        git_url: impl AsRef<str>,
+        branch: &Option<String>,
+        spin_version: &str,
+    ) -> anyhow::Result<Self> {
         let url_str = git_url.as_ref();
         let url =
             Url::parse(url_str).with_context(|| format!("Failed to parse {} as URL", url_str))?;
-        Ok(Self::Git {
+        Ok(Self::Git(GitTemplateSource {
             url,
             branch: branch.clone(),
-        })
+            spin_version: spin_version.to_owned(),
+        }))
     }
 }
 
@@ -53,7 +66,7 @@ pub(crate) struct LocalTemplateSource {
 impl TemplateSource {
     pub(crate) async fn get_local(&self) -> anyhow::Result<LocalTemplateSource> {
         match self {
-            Self::Git { url, branch } => clone_local(url, branch).await,
+            Self::Git(git_source) => clone_local(git_source).await,
             Self::File(path) => check_local(path).await,
         }
     }
@@ -86,20 +99,28 @@ impl LocalTemplateSource {
     }
 }
 
-async fn clone_local(url: &Url, branch: &Option<String>) -> anyhow::Result<LocalTemplateSource> {
+async fn clone_local(git_source: &GitTemplateSource) -> anyhow::Result<LocalTemplateSource> {
     let temp_dir = tempdir()?;
     let path = temp_dir.path().to_owned();
 
-    let url_str = url.as_str();
+    let url_str = git_source.url.as_str();
+
+    let actual_branch = match &git_source.branch {
+        Some(b) => Some(b.clone()),
+        None => version_matched_tag(url_str, &git_source.spin_version).await,
+    };
 
     let mut git = Command::new("git");
     git.arg("clone");
+    git.arg("--depth").arg("1");
 
-    if let Some(b) = branch {
+    if let Some(b) = actual_branch {
         git.arg("--branch").arg(b);
     }
 
-    let clone_result = git.arg(&url_str).arg(&path).output().await?;
+    git.arg(&url_str).arg(&path);
+
+    let clone_result = git.output().await?;
     match clone_result.status.success() {
         true => Ok(LocalTemplateSource {
             root: path,
@@ -114,6 +135,32 @@ async fn clone_local(url: &Url, branch: &Option<String>) -> anyhow::Result<Local
     }
 }
 
+async fn version_matched_tag(url: &str, spin_version: &str) -> Option<String> {
+    let preferred_tag = version_preferred_tag(spin_version);
+
+    let mut git = Command::new("git");
+    git.arg("ls-remote");
+    git.arg("--exit-code");
+    git.arg(&url);
+    git.arg(&preferred_tag);
+
+    git.output().await.ok().and_then(|output| {
+        if output.status.success() {
+            Some(preferred_tag)
+        } else {
+            None
+        }
+    })
+}
+
+fn version_preferred_tag(text: &str) -> String {
+    let mm_version = match semver::Version::parse(text) {
+        Ok(version) => format!("{}.{}", version.major, version.minor),
+        Err(_) => text.to_owned(),
+    };
+    format!("{}{}", TEMPLATE_VERSION_TAG_PREFIX, mm_version)
+}
+
 async fn check_local(path: &Path) -> anyhow::Result<LocalTemplateSource> {
     if path.exists() {
         Ok(LocalTemplateSource {
@@ -122,5 +169,38 @@ async fn check_local(path: &Path) -> anyhow::Result<LocalTemplateSource> {
         })
     } else {
         Err(anyhow!("Path not found: {}", path.display()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn preferred_tag_excludes_patch_version() {
+        assert_eq!("spin/templates/v1.2", version_preferred_tag("1.2.3"));
+    }
+
+    #[test]
+    fn preferred_tag_excludes_prerelease_and_build() {
+        assert_eq!(
+            "spin/templates/v1.2",
+            version_preferred_tag("1.2.3-preview.1")
+        );
+        assert_eq!(
+            "spin/templates/v1.2",
+            version_preferred_tag("1.2.3+build.0f74628")
+        );
+        assert_eq!(
+            "spin/templates/v1.2",
+            version_preferred_tag("1.2.3-alpha+0f74628")
+        );
+    }
+
+    #[test]
+    fn preferred_tag_defaults_sensibly_on_bad_semver() {
+        assert_eq!("spin/templates/v1.2", version_preferred_tag("1.2"));
+        assert_eq!("spin/templates/v1.2.3.4", version_preferred_tag("1.2.3.4"));
+        assert_eq!("spin/templates/vgarbage", version_preferred_tag("garbage"));
     }
 }

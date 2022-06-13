4c3b1976cd09e96a52162001bd57cb6612fae92a
elias <elias.jm.garcia@gmail.com>
2022-06-12 22:01:50 -0500

    initial changes to headers-env-routes integration test
    Signed-off-by: elias <elias.jm.garcia@gmail.com>

--- tests/http/headers-env-routes-test/src/lib.rs
+++ tests/http/headers-env-routes-test/src/lib.rs
@@ -1,41 +1,39 @@
-// Import the HTTP objects from the generated bindings.
-use spin_http::{Request, Response};
+use anyhow::Result;
+use spin_sdk::{http::{Request, Response}, http_component};
+use std::collections::HashMap;
+use std::convert::TryInto;
 
-// Generate Rust bindings for interface defined in spin-http.wit file
-wit_bindgen_rust::export!("spin-http.wit");
+#[http_component]
+fn hello_world(mut req: Request) -> Result<Response> {
+    let headers = env_to_headers();
+    let req_headers = req.headers_mut();
+    // NOTE: The change between the old code's append_request_headers function and that provided by the Rust SDK don't line up as far as I can tell and that lead me
+    //       into a minor fight with the compiler. The code below feels like a mess and should be looked at with some scrutiny.
+    let mut name_map = HashMap::new();
+    let _ = headers
+        .into_iter()
+        .map(|(k,v)| name_map.insert(k, v));
+    let _ = req_headers
+        .iter()
+        .map(|(k, v)| name_map.insert(String::from(k.as_str()), String::from(v.to_str().unwrap())));
+    
+    let res_headers_map : http::HeaderMap = (&name_map).try_into()?;
+    
+    let mut res = http::Response::builder()
+        .status(200);
+    
+    let res_headers = res.headers_mut().unwrap();
 
-struct SpinHttp;
-impl spin_http::SpinHttp for SpinHttp {
-    // Implement the `handler` entrypoint for Spin HTTP components.
-    // This handler does the following:
-    // - returns all environment variables as headers with an ENV_ prefix
-    // - returns all request headers as response headers.
-    fn handle_http_request(req: Request) -> Response {
-        let mut headers = Self::env_to_headers();
-        Self::append_request_headers(&mut headers, &req.headers);
-        let headers = Some(headers);
-        Response {
-            status: 200,
-            headers,
-            body: Some("I'm a teapot".as_bytes().to_vec()),
-        }
-    }
+    let _ = res_headers_map
+        .iter()
+        .map(|(k,v)| res_headers.insert(k, http::HeaderValue::clone(v)));
+
+    Ok(res.body(Some("I'm a teapot".into()))?)
 }
 
-impl SpinHttp {
-    fn env_to_headers() -> Vec<(String, String)> {
-        let mut res = vec![];
-        std::env::vars().for_each(|(k, v)| res.push((format!("ENV_{}", k), v)));
+fn env_to_headers() -> Vec<(String, String)> {
+    let mut res = vec![];
+    std::env::vars().for_each(|(k, v)| res.push((format!("ENV_{}", k), v)));
 
-        res
-    }
-
-    fn append_request_headers(
-        res_headers: &mut Vec<(String, String)>,
-        req_headers: &[(String, String)],
-    ) {
-        for (k, v) in req_headers {
-            res_headers.push((k.clone(), v.clone()));
-        }
-    }
+    res
 }

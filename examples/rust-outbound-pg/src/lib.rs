9907142faa51b063b6ec04a414705a1b167a38da
Mike Tang <daogangtang@gmail.com>
2022-07-20 15:43:21 +0800

    Add usage of the retreived result from postgres.
    For that this is the first concept verified version, we didn't utilize the
    library like postgres-types and serde.
    
    Signed-off-by: Mike Tang <daogangtang@gmail.com>

--- examples/rust-outbound-pg/src/lib.rs
+++ examples/rust-outbound-pg/src/lib.rs
@@ -1,6 +1,7 @@
+#![allow(dead_code)]
 use anyhow::{anyhow, Result};
 use spin_sdk::{
-    http::{internal_server_error, Request, Response},
+    http::{Request, Response},
     http_component, pg,
 };
 
@@ -8,14 +9,32 @@ use spin_sdk::{
 // address of the Pg server that the component will write to
 const DB_URL_ENV: &str = "DB_URL";
 
+#[derive(Debug, Clone)]
+struct Article {
+    title: String, 
+    content: String,
+    authorname: String,
+}
+
 #[http_component]
 fn read(_req: Request) -> Result<Response> {
     let address = std::env::var(DB_URL_ENV)?;
 
-    let sql = "select * from articletest";
-    let rows = pg::query(&address,sql, &vec![]).map_err(|_| anyhow!("Error execute pg command"))?;
+    let sql = "select title, content, authorname from articletest";
+    let rows = pg::query(&address,sql, &[]).map_err(|_| anyhow!("Error execute pg command"))?;
 
     println!("rows: {:?}", rows);
+    for row in rows {
+        let title = String::from_utf8(row[0].clone())?;
+        let content = String::from_utf8(row[1].clone())?;
+        let authorname = String::from_utf8(row[2].clone())?;
+
+        let article = Article {title, content, authorname};
+
+        println!("article: {:#?}", article);
+    }
+    
+    // use it in business logic
 
     Ok(http::Response::builder().status(200).body(None)?)
 }

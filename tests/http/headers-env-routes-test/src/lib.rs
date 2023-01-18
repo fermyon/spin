ea4a98d8b3943d21f44774825f9b6f0e37337f97
Vaughn Dice <vaughn.dice@fermyon.com>
2023-01-18 10:54:18 -0700

    ref(tests): update headers-env-routes-test to use the SDK
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- tests/http/headers-env-routes-test/src/lib.rs
+++ tests/http/headers-env-routes-test/src/lib.rs
@@ -1,41 +1,29 @@
-// Import the HTTP objects from the generated bindings.
-use spin_http::{Request, Response};
+use anyhow::Result;
+use spin_sdk::{
+    http::{Request, Response},
+    http_component
+};
 
-// Generate Rust bindings for interface defined in spin-http.wit file
-wit_bindgen_rust::export!("spin-http.wit");
-
-struct SpinHttp;
-impl spin_http::SpinHttp for SpinHttp {
-    // Implement the `handler` entrypoint for Spin HTTP components.
-    // This handler does the following:
-    // - returns all environment variables as headers with an ENV_ prefix
-    // - returns all request headers as response headers.
-    fn handle_http_request(req: Request) -> Response {
-        let mut headers = Self::env_to_headers();
-        Self::append_request_headers(&mut headers, &req.headers);
-        let headers = Some(headers);
-        Response {
-            status: 200,
-            headers,
-            body: Some("I'm a teapot".as_bytes().to_vec()),
-        }
+// This handler does the following:
+// - returns all environment variables as headers with an ENV_ prefix
+// - returns all request headers as response headers.
+#[http_component]
+fn handle_http_request(req: Request) -> Result<Response> {
+    match append_headers(http::Response::builder(), &req) {
+        Err(e) => anyhow::bail!("Unable to append headers to response: {}", e),
+        Ok(resp) => Ok(resp
+            .status(200)
+            .body(Some("I'm a teapot".into()))?)
     }
 }
 
-impl SpinHttp {
-    fn env_to_headers() -> Vec<(String, String)> {
-        let mut res = vec![];
-        std::env::vars().for_each(|(k, v)| res.push((format!("ENV_{}", k), v)));
-
-        res
+fn append_headers(mut resp: http::response::Builder, req: &Request) -> Result<http::response::Builder> {
+    for (k, v) in std::env::vars() {
+        resp = resp.header(format!("ENV_{}", k), v);
+    }
+    for (k, v) in req.headers().iter() {
+        resp = resp.header(k, v);
     }
 
-    fn append_request_headers(
-        res_headers: &mut Vec<(String, String)>,
-        req_headers: &[(String, String)],
-    ) {
-        for (k, v) in req_headers {
-            res_headers.push((k.clone(), v.clone()));
-        }
-    }
+    Ok(resp)
 }

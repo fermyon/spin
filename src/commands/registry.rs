e3bfed785c25447a46408e9e73ca8ccc7ab0121f
Radu Matei <radu.matei@fermyon.com>
2023-02-15 16:15:40 +0000

    Implement `spin registry login`
    This commit implements functionality for logging in to an OCI registry.
    Until this commit, a user would have had to use external tooling to log
    in to a target registry (such as `docker login` or other registry
    specific tools). This had the advantage that users who already had a
    setup for working with container regsitries could continue to use it,
    but it introduced a dependency on external tools for pushing a Spin
    application.
    
    This commit implements the functionality to login to a registry natively
    into Spin.
    Specifically, it adds this a `spin regsitry login` command:
    
    ```
    $ spin registry login --help
    Log in to a registry
    
    USAGE:
        spin registry login [OPTIONS] <SERVER>
    
    ARGS:
        <SERVER>
    
    OPTIONS:
        -h, --help                   Print help information
        -p, --password <PASSWORD>    Password for the container registry
            --password-stdin         Take the password from stdin
        -u, --username <USERNAME>    Username for the container registry
    ```
    
    This command mirrors existing tools that perform login operations to
    registries (flags for the username and passwords, flag to read the
    password from standard in that takes precedence).
    
    The credentials are base64 encoded (standard for container tools that do
    not use Docker's credential helpers) and written to
    `$XDG_CONFIG_HOME/fermyon/registry-auth.json` (side-by-side with the
    Fermyon Cloud authentication file).
    
    Functionality that needs to work with a registry (`spin registry
    push/pull`, `spin up`) will first search the Spin configuration for the
    credentials, then will fallback to searching the authenticated
    registries in the Docker configuration file.
    
    Examples:
    
    ```
    $ echo $GHCR_PAT | spin registry login --username radu-matei ghcr.io --password-stdin
    Successfully authenticated as radu-matei to registry ghcr.io
    
    $ spin registry login --username radumatei --password <password> https://index.docker.io
    Successfully authenticated as radumatei to registry https://index.docker.io
    
    $ spin registry login index.docker.io
    Username:
    radumatei
    Password: (prompt, hidden input)
    
    Successfully authenticated as radumatei to registry https://index.docker.io
    ```
    
    This functionality should make `spin registry login` a drop-in
    replacement for most of the existing tools that login to registries,
    while continuing to reuse existing credentials as a fallback.
    
    A future commit needs to make writing the configuration file more
    flexible, and add unit tests for decoding credentials.
    
    Signed-off-by: Radu Matei <radu.matei@fermyon.com>

--- src/commands/registry.rs
+++ src/commands/registry.rs
@@ -1,6 +1,7 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
-use std::path::PathBuf;
+use spin_publish::oci::client::Client;
+use std::{io::Read, path::PathBuf};
 
 use crate::opts::*;
 
@@ -10,10 +11,12 @@ use crate::opts::*;
 /// authenticate to registries.
 #[derive(Subcommand, Debug)]
 pub enum RegistryCommands {
-    /// Push a Spin application to an OCI registry.
+    /// Push a Spin application to a registry.
     Push(Push),
-    /// Pull a Spin application from an OCI registry.
+    /// Pull a Spin application from a registry.
     Pull(Pull),
+    /// Log in to a registry.
+    Login(Login),
 }
 
 impl RegistryCommands {
@@ -21,6 +24,7 @@ impl RegistryCommands {
         match self {
             RegistryCommands::Push(cmd) => cmd.run().await,
             RegistryCommands::Pull(cmd) => cmd.run().await,
+            RegistryCommands::Login(cmd) => cmd.run().await,
         }
     }
 }
@@ -87,6 +91,74 @@ impl Pull {
         let mut client = spin_publish::oci::client::Client::new(self.insecure, None).await?;
         client.pull(&self.reference).await?;
 
+        Ok(())
+    }
+}
+
+#[derive(Parser, Debug)]
+pub struct Login {
+    /// Username for the registry
+    #[clap(long = "username", short = 'u')]
+    pub username: Option<String>,
+
+    /// Password for the registry
+    #[clap(long = "password", short = 'p')]
+    pub password: Option<String>,
+
+    /// Take the password from stdin
+    #[clap(
+        long = "password-stdin",
+        takes_value = false,
+        conflicts_with = "password"
+    )]
+    pub password_stdin: bool,
+
+    #[clap()]
+    pub server: String,
+}
+
+impl Login {
+    pub async fn run(self) -> Result<()> {
+        let username = match self.username {
+            Some(u) => u,
+            None => {
+                let prompt = "Username";
+                loop {
+                    let result = dialoguer::Input::<String>::new()
+                        .with_prompt(prompt)
+                        .interact_text()?;
+                    if result.trim().is_empty() {
+                        continue;
+                    } else {
+                        break result;
+                    }
+                }
+            }
+        };
+
+        // If the --password-stdin flag is passed, read the password from standard input.
+        // Otherwise, if the --password flag was passed with a value, use that value. Finally, if
+        // neither was passed, prompt the user to input the password.
+        let password = if self.password_stdin {
+            let mut buf = String::new();
+            let mut stdin = std::io::stdin().lock();
+            stdin.read_to_string(&mut buf)?;
+            buf
+        } else {
+            match self.password {
+                Some(p) => p,
+                None => rpassword::prompt_password("Password: ")?,
+            }
+        };
+
+        Client::login(&self.server, &username, &password)
+            .await
+            .context("cannot log in to the registry")?;
+
+        println!(
+            "Successfully logged in as {} to registry {}",
+            username, &self.server
+        );
         Ok(())
     }
 }

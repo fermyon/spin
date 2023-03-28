03f39e8cbee4ab38e64cec97a3ad9f62cff2aac7
Joel Dice <joel.dice@fermyon.com>
2023-03-28 13:37:32 -0600

    fix `spin-timer` example and add a test so it stays fixed (#1331)
    * fix `spin-timer` example and add a test so it stays fixed
    
    This primarily required updating both the trigger and the
    `app-example` that exercises it to use the latest, component-oriented,
    binding generators.
    
    Eventually, I'd like to see _all_ the examples tested (or at least
    built) via either integration or end-to-end tests.  Consider this the
    first step in that direction.
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>
    
    * change `unwrap`s to `?`s in `test_timer_trigger`
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>
    
    ---------
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- build.rs
+++ build.rs
@@ -11,6 +11,7 @@ const RUST_HTTP_INTEGRATION_TEST: &str = "tests/http/simple-spin-rust";
 const RUST_HTTP_INTEGRATION_ENV_TEST: &str = "tests/http/headers-env-routes-test";
 const RUST_HTTP_VAULT_CONFIG_TEST: &str = "tests/http/vault-config-test";
 const RUST_OUTBOUND_REDIS_INTEGRATION_TEST: &str = "tests/outbound-redis/http-rust-outbound-redis";
+const TIMER_TRIGGER_INTEGRATION_TEST: &str = "examples/spin-timer/app-example";
 
 fn main() {
     let mut config = vergen::Config::default();
@@ -78,6 +79,7 @@ error: the `wasm32-wasi` target is not installed
     cargo_build(RUST_HTTP_INTEGRATION_ENV_TEST);
     cargo_build(RUST_HTTP_VAULT_CONFIG_TEST);
     cargo_build(RUST_OUTBOUND_REDIS_INTEGRATION_TEST);
+    cargo_build(TIMER_TRIGGER_INTEGRATION_TEST);
 }
 
 fn build_wasm_test_program(name: &'static str, root: &'static str) {

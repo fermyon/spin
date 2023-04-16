efa5feb90c8d963cc1484fad58340bf37f83f4fa
Thorsten Hans <thorsten.hans@gmail.com>
2023-04-16 17:08:18 +0200

    chore(sdk): Add options fn to register handlers for OPTIONS requests
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- sdk/rust/src/http/router.rs
+++ sdk/rust/src/http/router.rs
@@ -158,6 +158,14 @@ impl Router {
         self.add(path, http_types::Method::PATCH, handler)
     }
 
+    /// Register a handler at the path for the HTTP OPTIONS method.
+    pub fn options<F>(&mut self, path: &str, handler: F)
+    where
+        F: Fn(Request, Params) -> Result<Response> + 'static,
+    {
+        self.add(path, http_types::Method::OPTIONS, handler)
+    }
+
     /// Construct a new Router.
     pub fn new() -> Self {
         Router {

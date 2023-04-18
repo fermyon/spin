eef4472fac5c5ece30a8e14edc07aa527d8193a1
Adam Reese <adam@reese.io>
2023-04-18 12:44:43 -0700

    ref(sdk/go): remove unused custom error type
    - Remove unused `Error` type
    - Remove exported error consts.  These are set for the internal error
      code from spin.  The error codes aren't exposed to the user.
    - Fix shadowing of the `string` type
    
    The types removed are exported and part of the api but are no use.
    I think it's safe to remove them. _last words_
    
    Signed-off-by: Adam Reese <adam@reese.io>

--- sdk/go/key_value/key-value.go
+++ sdk/go/key_value/key-value.go
@@ -4,27 +4,20 @@ package key_value
 import "C"
 import (
 	"errors"
-	"unsafe"
 	"fmt"
+	"unsafe"
 )
 
 type Store C.key_value_store_t
 
-type ErrorKind C.uint8_t
-
 const (
-	ErrorKindStoreTableFull = iota
-	ErrorKindNoSuchStore
-	ErrorKindAccessDenied
-	ErrorKindInvalidStore
-	ErrorKindNoSuchKey
-	ErrorKindIo
+	errorKindStoreTableFull = iota
+	errorKindNoSuchStore
+	errorKindAccessDenied
+	errorKindInvalidStore
+	errorKindNoSuchKey
+	errorKindIo
 )
-
-type Error struct {
-	Kind ErrorKind
-	Val interface{}
-}
 
 func Open(name string) (Store, error) {
 	cname := toCStr(name)
@@ -32,9 +25,8 @@ func Open(name string) (Store, error) {
 	C.key_value_open(&cname, &ret)
 	if ret.is_err {
 		return 0xFFFF_FFFF, toErr((*C.key_value_error_t)(unsafe.Pointer(&ret.val)))
-	} else {
-		return *(*Store)(unsafe.Pointer(&ret.val)), nil
 	}
+	return *(*Store)(unsafe.Pointer(&ret.val)), nil
 }
 
 func Get(store Store, key string) ([]byte, error) {
@@ -43,10 +35,9 @@ func Get(store Store, key string) ([]byte, error) {
 	C.key_value_get(C.uint32_t(store), &ckey, &ret)
 	if ret.is_err {
 		return []byte{}, toErr((*C.key_value_error_t)(unsafe.Pointer(&ret.val)))
-	} else {
-		list := (*C.key_value_list_u8_t)(unsafe.Pointer(&ret.val))
-		return C.GoBytes(unsafe.Pointer(list.ptr), C.int(list.len)), nil
 	}
+	list := (*C.key_value_list_u8_t)(unsafe.Pointer(&ret.val))
+	return C.GoBytes(unsafe.Pointer(list.ptr), C.int(list.len)), nil
 }
 
 func Set(store Store, key string, value []byte) error {
@@ -56,9 +47,8 @@ func Set(store Store, key string, value []byte) error {
 	C.key_value_set(C.uint32_t(store), &ckey, &cbytes, &ret)
 	if ret.is_err {
 		return toErr((*C.key_value_error_t)(unsafe.Pointer(&ret.val)))
-	} else {
-		return nil
 	}
+	return nil
 }
 
 func Delete(store Store, key string) error {
@@ -67,9 +57,8 @@ func Delete(store Store, key string) error {
 	C.key_value_delete(C.uint32_t(store), &ckey, &ret)
 	if ret.is_err {
 		return toErr((*C.key_value_error_t)(unsafe.Pointer(&ret.val)))
-	} else {
-		return nil
 	}
+	return nil
 }
 
 func Exists(store Store, key string) (bool, error) {
@@ -78,9 +67,8 @@ func Exists(store Store, key string) (bool, error) {
 	C.key_value_exists(C.uint32_t(store), &ckey, &ret)
 	if ret.is_err {
 		return false, toErr((*C.key_value_error_t)(unsafe.Pointer(&ret.val)))
-	} else {
-		return *(*bool)(unsafe.Pointer(&ret.val)), nil
 	}
+	return *(*bool)(unsafe.Pointer(&ret.val)), nil
 }
 
 func GetKeys(store Store) ([]string, error) {
@@ -88,9 +76,8 @@ func GetKeys(store Store) ([]string, error) {
 	C.key_value_get_keys(C.uint32_t(store), &ret)
 	if ret.is_err {
 		return []string{}, toErr((*C.key_value_error_t)(unsafe.Pointer(&ret.val)))
-	} else {
-		return fromCStrList((*C.key_value_list_string_t)(unsafe.Pointer(&ret.val))), nil
 	}
+	return fromCStrList((*C.key_value_list_string_t)(unsafe.Pointer(&ret.val))), nil
 }
 
 func Close(store Store) {
@@ -111,8 +98,8 @@ func fromCStrList(list *C.key_value_list_string_t) []string {
 
 	slice := unsafe.Slice(list.ptr, listLen)
 	for i := 0; i < listLen; i++ {
-		string := slice[i]
-		result = append(result, C.GoStringN(string.ptr, C.int(string.len)))
+		str := slice[i]
+		result = append(result, C.GoStringN(str.ptr, C.int(str.len)))
 	}
 
 	return result
@@ -120,15 +107,20 @@ func fromCStrList(list *C.key_value_list_string_t) []string {
 
 func toErr(error *C.key_value_error_t) error {
 	switch error.tag {
-	case ErrorKindStoreTableFull: return errors.New("store table full")
-	case ErrorKindNoSuchStore: return errors.New("no such store")
-	case ErrorKindAccessDenied: return errors.New("access denied")
-	case ErrorKindInvalidStore: return errors.New("invalid store")
-	case ErrorKindNoSuchKey: return errors.New("no such key")
-	case ErrorKindIo: {
-		string := (*C.key_value_string_t)(unsafe.Pointer(&error.val))
-		return errors.New(fmt.Sprintf("io error: %s", C.GoStringN(string.ptr, C.int(string.len))))
-	}
-	default: return errors.New(fmt.Sprintf("unrecognized error: %v", error.tag))
+	case errorKindStoreTableFull:
+		return errors.New("store table full")
+	case errorKindNoSuchStore:
+		return errors.New("no such store")
+	case errorKindAccessDenied:
+		return errors.New("access denied")
+	case errorKindInvalidStore:
+		return errors.New("invalid store")
+	case errorKindNoSuchKey:
+		return errors.New("no such key")
+	case errorKindIo:
+		str := (*C.key_value_string_t)(unsafe.Pointer(&error.val))
+		return errors.New(fmt.Sprintf("io error: %s", C.GoStringN(str.ptr, C.int(str.len))))
+	default:
+		return errors.New(fmt.Sprintf("unrecognized error: %v", error.tag))
 	}
 }

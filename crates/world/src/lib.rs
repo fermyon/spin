0c486b0a8ce5e0458e0dc6952aab13bc942026a3
Lann Martin <lann.martin@fermyon.com>
2023-06-13 17:31:24 -0400

    split world prototype
    Signed-off-by: Lann Martin <lann.martin@fermyon.com>

--- crates/world/src/lib.rs
+++ crates/world/src/lib.rs
@@ -2,6 +2,50 @@
 
 wasmtime::component::bindgen!({
     path: "../../wit/preview2",
-    world: "reactor",
-    async: true
+    async: true,
+    interfaces: "
+        import config: spin.config
+        import postgres: spin.postgres
+        import mysql: spin.mysql
+        import sqlite: spin.sqlite
+        import redis: spin.redis
+        import key-value: spin.key-value
+        import http: spin.http
+    ",
+});
+
+wasmtime::component::bindgen!({
+    world: "trigger-http",
+    path: "../../wit/preview2",
+    async: true,
+    with: {
+        "config": config,
+        "http": http,
+        "http_types": crate::http_types,
+        "key_value": key_value,
+        "mysql": mysql,
+        "postgres": postgres,
+        "rdbms_types": rdbms_types,
+        "redis": redis,
+        "redis_types": redis_types,
+        "sqlite": sqlite,
+    },
+});
+
+wasmtime::component::bindgen!({
+    world: "trigger-redis",
+    path: "../../wit/preview2",
+    async: true,
+    with: {
+        "config": config,
+        "http": http,
+        "http_types": http_types,
+        "key_value": key_value,
+        "mysql": mysql,
+        "postgres": postgres,
+        "rdbms_types": rdbms_types,
+        "redis": redis,
+        "redis_types": crate::redis_types,
+        "sqlite": sqlite,
+    },
 });

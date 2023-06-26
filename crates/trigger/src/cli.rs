5cba19aed7654b51f9c4e7ebff174de90a1c70f5
itowlson <ivan.towlson@fermyon.com>
2023-06-27 10:11:40 +1200

    Move Rust SDK SQLite out of `experimental` feature
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- crates/trigger/src/cli.rs
+++ crates/trigger/src/cli.rs
@@ -108,7 +108,7 @@ where
     key_values: Vec<(String, String)>,
 
     /// Run a SQLite statement such as a migration against the default database.
-    /// To run from a file, prefix the filename with @ e.g. spin up --sqlite @migration1.sql
+    /// To run from a file, prefix the filename with @ e.g. spin up --sqlite @migration.sql
     #[clap(long = "sqlite")]
     sqlite_statements: Vec<String>,
 

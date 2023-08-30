a8d048d3c2c76dda3835fd66ad25ff9263aa1caf
Alex Crichton <alex@alexcrichton.com>
2023-08-30 15:20:44 -0700

    Update HTTP benchmarks
    This commit is an attempt at enhancing the HTTP benchmarks in-repo with
    some more flavors in addition to changing what's being measured. One of
    the important vectors measured previously was the effect of concurrent
    requests on latency, but the measurement reported was one of handling an
    entire batch of requests. Instead this commit now reimplements this in
    terms of what the latency of a single request is with N units of
    background work.
    
    Benchmarks will now, before running, spin up background work performing
    concurrent requests. The benchmark then executes, measuring the latency
    of a single request while there's background work. When the measurement
    is over all background requests are torn down.
    
    Additionally this commit fixes the processing of the `sleep` parameter
    in the `spin-http-benchmark` crate since the `params` field it
    previously read is no longer set.
    
    Signed-off-by: Alex Crichton <alex@alexcrichton.com>

--- crates/trigger-http/benches/baseline.rs
+++ crates/trigger-http/benches/baseline.rs
@@ -1,15 +1,14 @@
+use std::future::Future;
+use std::sync::atomic::{AtomicBool, Ordering::Relaxed};
 use std::sync::Arc;
-use std::time::Instant;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
-use futures::future::join_all;
 use http::uri::Scheme;
 use http::Request;
 use spin_testing::{assert_http_response_success, HttpTestConfig};
 use spin_trigger_http::HttpTrigger;
 use tokio::runtime::Runtime;
-use tokio::task;
 
 criterion_main!(benches);
 criterion_group!(
@@ -19,6 +18,26 @@ criterion_group!(
     bench_wagi_concurrency_minimal,
 );
 
+async fn spin_trigger() -> Arc<HttpTrigger> {
+    Arc::new(
+        HttpTestConfig::default()
+            .test_program("spin-http-benchmark.wasm")
+            .http_spin_trigger("/")
+            .build_trigger()
+            .await,
+    )
+}
+
+async fn wagi_trigger() -> Arc<HttpTrigger> {
+    Arc::new(
+        HttpTestConfig::default()
+            .test_program("wagi-benchmark.wasm")
+            .http_wagi_trigger("/", Default::default())
+            .build_trigger()
+            .await,
+    )
+}
+
 // Benchmark time to start and process one request
 fn bench_startup(c: &mut Criterion) {
     let async_runtime = Runtime::new().unwrap();
@@ -26,95 +45,56 @@ fn bench_startup(c: &mut Criterion) {
     let mut group = c.benchmark_group("startup");
     group.bench_function("spin-executor", |b| {
         b.to_async(&async_runtime).iter(|| async {
-            let trigger = HttpTestConfig::default()
-                .test_program("spin-http-benchmark.wasm")
-                .http_spin_trigger("/")
-                .build_trigger()
-                .await;
-            run_concurrent_requests(Arc::new(trigger), 0, 1).await;
+            let trigger = spin_trigger().await;
+            run(&trigger, "/").await;
         });
     });
     group.bench_function("spin-wagi-executor", |b| {
         b.to_async(&async_runtime).iter(|| async {
-            let trigger = HttpTestConfig::default()
-                .test_program("wagi-benchmark.wasm")
-                .http_wagi_trigger("/", Default::default())
-                .build_trigger()
-                .await;
-            run_concurrent_requests(Arc::new(trigger), 0, 1).await;
+            let trigger = wagi_trigger().await;
+            run(&trigger, "/").await;
         });
     });
 }
 
-// Benchmark SpinHttpExecutor time to process requests at various levels of concurrency
 fn bench_spin_concurrency_minimal(c: &mut Criterion) {
-    let async_runtime = Runtime::new().unwrap();
-
-    let spin_trigger: Arc<HttpTrigger> = Arc::new(
-        async_runtime.block_on(
-            HttpTestConfig::default()
-                .test_program("spin-http-benchmark.wasm")
-                .http_spin_trigger("/")
-                .build_trigger(),
-        ),
-    );
-
-    let sleep_ms = 1;
-    let mut group = c.benchmark_group(format!("spin-executor/sleep-{}ms", sleep_ms));
-    for concurrency in concurrency_steps() {
-        let bench_inner = || {
-            black_box(run_concurrent_requests(
-                spin_trigger.clone(),
-                sleep_ms,
-                concurrency,
-            ))
-        };
-
-        group.bench_function(format!("concurrency-{}", concurrency), |b| {
-            b.to_async(&async_runtime).iter_custom(|iters| async move {
-                let start = Instant::now();
-                for _ in 0..iters {
-                    bench_inner().await;
-                }
-                start.elapsed()
-            });
-        });
-    }
+    bench_concurrency_minimal(c, "spin-executor", spin_trigger);
+}
+fn bench_wagi_concurrency_minimal(c: &mut Criterion) {
+    bench_concurrency_minimal(c, "spin-wagi-executor", wagi_trigger);
 }
 
-// Benchmark WagiHttpExecutor time to process requests at various levels of concurrency
-fn bench_wagi_concurrency_minimal(c: &mut Criterion) {
+fn bench_concurrency_minimal<F: Future<Output = Arc<HttpTrigger>>>(
+    c: &mut Criterion,
+    name: &str,
+    mk: fn() -> F,
+) {
     let async_runtime = Runtime::new().unwrap();
+    let trigger = async_runtime.block_on(mk());
 
-    let wagi_trigger: Arc<HttpTrigger> = Arc::new(
-        async_runtime.block_on(
-            HttpTestConfig::default()
-                .test_program("wagi-benchmark.wasm")
-                .http_wagi_trigger("/", Default::default())
-                .build_trigger(),
-        ),
-    );
-
-    let sleep_ms = 1;
-    let mut group = c.benchmark_group(format!("spin-wagi-executor/sleep-{}ms", sleep_ms));
-    for concurrency in concurrency_steps() {
-        let bench_inner = || {
-            black_box(run_concurrent_requests(
-                wagi_trigger.clone(),
-                sleep_ms,
-                concurrency,
-            ))
-        };
-
-        group.bench_function(format!("concurrency-{}", concurrency), |b| {
-            b.to_async(&async_runtime).iter_custom(|iters| async move {
-                let start = Instant::now();
-                for _ in 0..iters {
-                    bench_inner().await;
+    for task in ["/?sleep=1", "/?noop", "/?cpu=1"] {
+        let mut group = c.benchmark_group(format!("{name}{task}"));
+        for concurrency in concurrency_steps() {
+            group.bench_function(format!("concurrency-{}", concurrency), |b| {
+                let done = Arc::new(AtomicBool::new(false));
+                let background = (0..concurrency - 1)
+                    .map(|_| {
+                        let trigger = trigger.clone();
+                        let done = done.clone();
+                        async_runtime.spawn(async move {
+                            while !done.load(Relaxed) {
+                                run(&trigger, task).await;
+                            }
+                        })
+                    })
+                    .collect::<Vec<_>>();
+                b.to_async(&async_runtime).iter(|| run(&trigger, task));
+                done.store(true, Relaxed);
+                for task in background {
+                    async_runtime.block_on(task).unwrap();
                 }
-                start.elapsed()
             });
-        });
+        }
     }
 }
 
@@ -129,19 +109,13 @@ fn concurrency_steps() -> [u32; 3] {
     }
 }
 
-async fn run_concurrent_requests(trigger: Arc<HttpTrigger>, sleep_ms: u32, concurrency: u32) {
-    join_all((0..concurrency).map(|_| {
-        let trigger = trigger.clone();
-        task::spawn(async move {
-            let req = Request::get(format!("/?sleep={}", sleep_ms))
-                .body(Default::default())
-                .unwrap();
-            let resp = trigger
-                .handle(req, Scheme::HTTP, "127.0.0.1:55555".parse().unwrap())
-                .await
-                .unwrap();
-            assert_http_response_success(&resp);
-        })
-    }))
-    .await;
+async fn run(trigger: &HttpTrigger, path: &str) {
+    let req = Request::get(path.to_string())
+        .body(Default::default())
+        .unwrap();
+    let resp = trigger
+        .handle(req, Scheme::HTTP, "127.0.0.1:55555".parse().unwrap())
+        .await
+        .unwrap();
+    assert_http_response_success(&resp);
 }

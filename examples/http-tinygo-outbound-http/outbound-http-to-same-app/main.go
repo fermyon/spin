c233253a49a746849c969560de5c6a56c0f34e52
Michelle Dhanani <michelle@fermyon.com>
2023-08-31 11:45:04 -0400

    add outbound http to same app tinygo example
    Signed-off-by: Michelle Dhanani <michelle@fermyon.com>

---
+++ examples/http-tinygo-outbound-http/outbound-http-to-same-app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	spinhttp "github.com/fermyon/spin/sdk/go/http"
+)
+
+func init() {
+	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		// Because we included self in `allowed_http_hosts`, we can make outbound
+		// HTTP requests to our own app using a relative path.
+		// This is not yet supported in cloud.
+		resp, err := spinhttp.Get("/hello")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintln(w, resp.Body)
+		fmt.Fprintln(w, resp.Header.Get("content-type"))
+	})
+}
+
+func main() {}
--- examples/http-tinygo-outbound-http/tinygo-hello/go.mod
+++ examples/http-tinygo-outbound-http/tinygo-hello/go.mod
@@ -6,4 +6,4 @@ require github.com/fermyon/spin/sdk/go v0.0.0
 
 require github.com/julienschmidt/httprouter v1.3.0 // indirect
 
-replace github.com/fermyon/spin/sdk/go v0.0.0 => ../../sdk/go/
+replace github.com/fermyon/spin/sdk/go v0.0.0 => ../../../sdk/go/

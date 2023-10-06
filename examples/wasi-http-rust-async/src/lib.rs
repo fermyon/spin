a834c373152e8013e43747e52d24198b9caa2455
Joel Dice <joel.dice@fermyon.com>
2023-10-06 16:38:54 -0600

    add `wasi_http_component` macro to Rust SDK
    The implementation is pretty ugly given that the inbound and outbound parts of
    `wasi-http` are tightly coupled, and it's not currently possible to tell
    `wit_bindgen::generate` to reuse types instead of generating duplicate ones.
    
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- examples/wasi-http-rust-async/src/lib.rs
+++ examples/wasi-http-rust-async/src/lib.rs
@@ -1,54 +1,20 @@
-wit_bindgen::generate!({
-    world: "wasi:http/proxy",
-    path: "wit",
-    exports: {
-        "wasi:http/incoming-handler": Component
-    }
-});
-
 use {
-    self::{
-        exports::wasi::http::incoming_handler::Guest as IncomingHandler,
-        wasi::{
-            http::{
-                outgoing_handler,
-                types::{
-                    self, Fields, IncomingBody, IncomingRequest, IncomingResponse, Method,
-                    OutgoingBody, OutgoingRequest, OutgoingResponse, ResponseOutparam, Scheme,
-                },
-            },
-            io::streams::StreamError,
-        },
+    self::wasi::http::types::{
+        Fields, IncomingRequest, Method, OutgoingBody, OutgoingRequest, OutgoingResponse,
+        ResponseOutparam, Scheme,
     },
-    anyhow::{anyhow, bail, Error, Result},
-    futures::{future, sink, stream, FutureExt, Sink, SinkExt, Stream, StreamExt, TryStreamExt},
+    anyhow::{bail, Result},
+    futures::{stream, FutureExt, SinkExt, StreamExt, TryStreamExt},
     sha2::{Digest, Sha256},
-    std::{cell::RefCell, future::Future, rc::Rc, str, task::Poll},
+    spin_sdk::wasi_http_component,
+    std::str,
     url::Url,
-    wakers::Wakers,
 };
 
-mod wakers;
-
-const READ_SIZE: u64 = 16 * 1024;
 const MAX_CONCURRENCY: usize = 16;
 
-struct Component;
-
-impl IncomingHandler for Component {
-    fn handle(request: IncomingRequest, response_out: ResponseOutparam) {
-        let wakers = Wakers::default();
-        let future = handle_async(wakers.clone(), request, response_out);
-        futures::pin_mut!(future);
-        wakers.run(future).unwrap();
-    }
-}
-
-async fn handle_async(
-    wakers: Wakers,
-    request: IncomingRequest,
-    response_out: ResponseOutparam,
-) -> Result<()> {
+#[wasi_http_component]
+async fn handle_request(request: IncomingRequest, response_out: ResponseOutparam) -> Result<()> {
     let method = request.method();
     let path = request.path_with_query();
     let headers = request.headers().entries();
@@ -62,10 +28,7 @@ async fn handle_async(
                     .and_then(|v| Url::parse(v).ok())
             });
 
-            let results = urls.map({
-                let wakers = wakers.clone();
-                move |url| hash(wakers.clone(), url.clone()).map(move |result| (url, result))
-            });
+            let results = urls.map(move |url| hash(url.clone()).map(move |result| (url, result)));
 
             let mut results = stream::iter(results).buffer_unordered(MAX_CONCURRENCY);
 
@@ -74,22 +37,20 @@ async fn handle_async(
                 &Fields::new(&[("content-type".to_string(), b"text/plain".to_vec())]),
             );
 
-            let mut sink = outgoing_response_body(wakers, &response);
+            let mut sink = executor::outgoing_response_body(&response);
 
             ResponseOutparam::set(response_out, Ok(response));
 
             while let Some((url, result)) = results.next().await {
-                sink.send(Some(
+                sink.send(
                     match result {
                         Ok(hash) => format!("{url}: {hash}\n"),
                         Err(e) => format!("{url}: {e:?}\n"),
                     }
                     .into_bytes(),
-                ))
+                )
                 .await?;
             }
-
-            sink.send(None).await?;
         }
 
         (Method::Post, Some("/echo")) => {
@@ -105,16 +66,14 @@ async fn handle_async(
                 ),
             );
 
-            let mut sink = outgoing_response_body(wakers.clone(), &response);
+            let mut sink = executor::outgoing_response_body(&response);
 
             ResponseOutparam::set(response_out, Ok(response));
 
-            let mut stream = incoming_request_body(wakers, request);
+            let mut stream = executor::incoming_request_body(request);
             while let Some(chunk) = stream.try_next().await? {
-                sink.send(Some(chunk)).await?;
+                sink.send(chunk).await?;
             }
-
-            sink.send(None).await?;
         }
 
         _ => {
@@ -131,7 +90,7 @@ async fn handle_async(
     Ok(())
 }
 
-async fn hash(wakers: Wakers, url: Url) -> Result<String> {
+async fn hash(url: Url) -> Result<String> {
     let request = OutgoingRequest::new(
         &Method::Get,
         Some(url.path()),
@@ -144,7 +103,7 @@ async fn hash(wakers: Wakers, url: Url) -> Result<String> {
         &Fields::new(&[]),
     );
 
-    let response = outgoing_request_send(wakers.clone(), request).await?;
+    let response = executor::outgoing_request_send(request).await?;
 
     let status = response.status();
 
@@ -152,7 +111,7 @@ async fn hash(wakers: Wakers, url: Url) -> Result<String> {
         bail!("unexpected status: {status}");
     }
 
-    let mut body = incoming_response_body(wakers, response);
+    let mut body = executor::incoming_response_body(response);
 
     let mut hasher = Sha256::new();
     while let Some(chunk) = body.try_next().await? {
@@ -160,148 +119,4 @@ async fn hash(wakers: Wakers, url: Url) -> Result<String> {
     }
 
     Ok(hex::encode(hasher.finalize()))
-}
-
-fn outgoing_response_body(
-    wakers: Wakers,
-    response: &OutgoingResponse,
-) -> impl Sink<Option<Vec<u8>>, Error = Error> {
-    outgoing_body(
-        wakers,
-        response.write().expect("response should be writable"),
-    )
-}
-
-fn outgoing_body(wakers: Wakers, body: OutgoingBody) -> impl Sink<Option<Vec<u8>>, Error = Error> {
-    let stream = body.write().expect("response body should be writable");
-    let pair = Rc::new(RefCell::new(Some((stream, body))));
-
-    sink::unfold((), {
-        move |(), chunk: Option<Vec<u8>>| {
-            future::poll_fn({
-                let mut offset = 0;
-                let wakers = wakers.clone();
-                let pair = pair.clone();
-
-                move |context| {
-                    if let Some(chunk) = chunk.as_ref() {
-                        let pair = pair.borrow();
-                        let (stream, _) = &pair.as_ref().unwrap();
-
-                        loop {
-                            assert!(!chunk[offset..].is_empty());
-
-                            match stream.check_write() {
-                                Ok(0) => {
-                                    wakers.insert(stream.subscribe(), context.waker().clone());
-                                    break Poll::Pending;
-                                }
-                                Ok(count) => {
-                                    let count =
-                                        usize::try_from(count).unwrap().min(chunk.len() - offset);
-
-                                    match stream.write(&chunk[offset..][..count]) {
-                                        Ok(()) => {
-                                            // TODO: only flush at end-of-stream
-                                            stream.flush().unwrap();
-                                            offset += count;
-                                            if offset == chunk.len() {
-                                                break Poll::Ready(Ok(()));
-                                            }
-                                        }
-                                        Err(_) => break Poll::Ready(Err(anyhow!("I/O error"))),
-                                    }
-                                }
-                                Err(_) => break Poll::Ready(Err(anyhow!("I/O error"))),
-                            }
-                        }
-                    } else {
-                        if let Some((stream, body)) = pair.borrow_mut().take() {
-                            drop(stream);
-                            OutgoingBody::finish(body, None);
-                        }
-                        Poll::Ready(Ok(()))
-                    }
-                }
-            })
-        }
-    })
-}
-
-fn outgoing_request_send(
-    wakers: Wakers,
-    request: OutgoingRequest,
-) -> impl Future<Output = Result<IncomingResponse, types::Error>> {
-    future::poll_fn({
-        let response = outgoing_handler::handle(request, None);
-
-        move |context| match &response {
-            Ok(response) => {
-                if let Some(response) = response.get() {
-                    Poll::Ready(response.unwrap())
-                } else {
-                    wakers.insert(response.subscribe(), context.waker().clone());
-                    Poll::Pending
-                }
-            }
-            Err(error) => Poll::Ready(Err(error.clone())),
-        }
-    })
-}
-
-fn incoming_request_body(
-    wakers: Wakers,
-    request: IncomingRequest,
-) -> impl Stream<Item = Result<Vec<u8>>> {
-    incoming_body(
-        wakers,
-        request.consume().expect("request should be consumable"),
-    )
-}
-
-fn incoming_response_body(
-    wakers: Wakers,
-    response: IncomingResponse,
-) -> impl Stream<Item = Result<Vec<u8>>> {
-    incoming_body(
-        wakers,
-        response.consume().expect("response should be consumable"),
-    )
-}
-
-fn incoming_body(wakers: Wakers, body: IncomingBody) -> impl Stream<Item = Result<Vec<u8>>> {
-    stream::poll_fn({
-        let stream = body.stream().expect("response body should be readable");
-        let mut pair = Some((stream, body));
-
-        move |context| {
-            let result = if let Some((stream, _)) = &pair {
-                match stream.read(READ_SIZE) {
-                    Ok(buffer) => {
-                        if buffer.is_empty() {
-                            wakers.insert(stream.subscribe(), context.waker().clone());
-                            Poll::Pending
-                        } else {
-                            Poll::Ready(Some(Ok(buffer)))
-                        }
-                    }
-                    Err(StreamError::Closed) => Poll::Ready(None),
-                    Err(StreamError::LastOperationFailed(error)) => {
-                        Poll::Ready(Some(Err(anyhow!("{}", error.to_debug_string()))))
-                    }
-                }
-            } else {
-                Poll::Ready(None)
-            };
-
-            if let Poll::Ready(None) = &result {
-                if let Some((stream, body)) = pair.take() {
-                    drop(stream);
-                    IncomingBody::finish(body);
-                }
-            }
-
-            result
-        }
-    })
 }

98a94f7afbc5552ff38d21de2a31dea08ac7b759
Brian H <brian.hardock@fermyon.com>
2023-10-25 15:24:50 -0600

    Fix broken template
    Signed-off-by: Brian H <brian.hardock@fermyon.com>

--- templates/http-rust/content/src/lib.rs
+++ templates/http-rust/content/src/lib.rs
@@ -1,10 +1,10 @@
-use spin_sdk::http::{IntoResponse, Request};
+use spin_sdk::http::{HeaderValue, IntoResponse, Request};
 use spin_sdk::http_component;
 
 /// A simple Spin HTTP component.
 #[http_component]
 fn handle_{{project-name | snake_case}}(req: Request) -> anyhow::Result<impl IntoResponse> {
-    println!("{:?}", req.headers);
+    println!("Handling request to {:?}", req.header("spin-full-url"));
     Ok(http::Response::builder()
         .status(200)
         .header("content-type", "text/plain")

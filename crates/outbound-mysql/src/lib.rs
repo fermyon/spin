95b08b8a604372d5a6ae46a93f5235f60382203d
Ryan Levick <ryan.levick@fermyon.com>
2023-10-30 10:44:50 +0100

    For postgres and mysql Connection::open- don't use v2 impl in v1
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- crates/outbound-mysql/src/lib.rs
+++ crates/outbound-mysql/src/lib.rs
@@ -18,6 +18,17 @@ pub struct OutboundMysql {
 }
 
 impl OutboundMysql {
+    async fn open_connection(&mut self, address: &str) -> Result<Resource<Connection>, v2::Error> {
+        self.connections
+            .push(
+                build_conn(address)
+                    .await
+                    .map_err(|e| v2::Error::ConnectionFailed(format!("{e:?}")))?,
+            )
+            .map_err(|_| v2::Error::ConnectionFailed("too many connections".into()))
+            .map(Resource::new_own)
+    }
+
     async fn get_conn(
         &mut self,
         connection: Resource<Connection>,
@@ -75,17 +86,7 @@ impl v2::HostConnection for OutboundMysql {
                 "address {address} is not permitted"
             ))));
         }
-        Ok(async {
-            self.connections
-                .push(
-                    build_conn(&address)
-                        .await
-                        .map_err(|e| v2::Error::ConnectionFailed(format!("{e:?}")))?,
-                )
-                .map_err(|_| v2::Error::ConnectionFailed("too many connections".into()))
-                .map(Resource::new_own)
-        }
-        .await)
+        Ok(self.open_connection(&address).await)
     }
 
     async fn execute(
@@ -158,7 +159,7 @@ macro_rules! delegate {
                 "address {} is not permitted", $address
             ))));
         }
-        let connection = match <Self as v2::HostConnection>::open($self, $address).await? {
+        let connection = match $self.open_connection(&$address).await {
             Ok(c) => c,
             Err(e) => return Ok(Err(e.into())),
         };

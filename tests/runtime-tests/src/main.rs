efe6802598bf165f791bf1cb8907ad969a049967
Ryan Levick <ryan.levick@fermyon.com>
2023-11-08 17:24:42 +0700

    Improve the runtime tests
    * Remove support for args and data (since this only applies to Spin and
      not other spin runtimes)
    * Improve test runner handling of output streams
    * Make helper crate for writing shared components
    
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- tests/runtime-tests/src/main.rs
+++ tests/runtime-tests/src/main.rs
@@ -12,33 +12,43 @@ fn main() -> anyhow::Result<()> {
         let test = test?;
         if test.file_type()?.is_dir() {
             log::info!("Testing: {}", test.path().display());
-            let temp = temp_dir::TempDir::new()?;
+            let temp = temp_dir::TempDir::new()
+                .context("failed to produce a temporary directory to run the test in")?;
             log::trace!("temporary directory: {}", temp.path().display());
             copy_manifest(&test.path(), &temp)?;
-            copy_data(&test.path(), &temp)?;
-            let args = get_args(&test.path())?;
-            log::info!("Starting Spin...");
-            let mut spin = Spin::start(&temp.path(), &args)?;
-            log::info!("Started Spin on port {}...", spin.port());
+            let mut spin = Spin::start(&temp.path())?;
+            log::debug!("Spin started on port {}.", spin.port());
 
-            let response: Response = make_http_request(&mut spin)?.parse()?;
+            // TODO: don't bail everything if the http request fails
+            let response = match make_http_request(&mut spin) {
+                Ok(r) => r,
+                Err(e) => {
+                    eprintln!("Test failed trying to connect to http server: {e}");
+                    continue;
+                }
+            };
+            let response: Response = response.parse()?;
+            let error_file = test.path().join("error.txt");
             match response {
-                Response::Ok => println!("Test passed!"),
-                Response::Err(e) => {
-                    let error_file = test.path().join("error.txt");
-                    if error_file.exists() {
-                        let error = std::fs::read_to_string(&error_file)?;
-                        if e.contains(&error) {
-                            println!("Test passed!");
-                        } else {
-                            eprintln!("Test errored but not in the expected way: {e}")
-                        }
+                Response::Ok if !error_file.exists() => println!("Test passed!"),
+                Response::Ok => {
+                    let expected = std::fs::read_to_string(&error_file)?;
+                    println!("Test passed but should have failed with error: {expected}")
+                }
+                Response::Err(e) if error_file.exists() => {
+                    let expected = std::fs::read_to_string(&error_file)
+                        .context("failed to read error.txt file")?;
+                    if e.contains(&expected) {
+                        println!("Test passed!");
                     } else {
-                        eprintln!("Test errored: {e}");
+                        eprintln!("Test errored but not in the expected way.\n\texpected: {expected}\n\tgot: {e}")
                     }
                 }
+                Response::Err(e) => {
+                    eprintln!("Test errored: {e}");
+                }
                 Response::ErrNoBody => {
-                    let stderr = spin.kill()?;
+                    let stderr = spin.stderr.output_as_str().unwrap_or("<non-utf8>");
                     eprintln!("Spin did not return error body (most likely due to a test panicking). stderr:\n{stderr}");
                 }
             }
@@ -47,29 +57,6 @@ fn main() -> anyhow::Result<()> {
         }
     }
     Ok(())
-}
-
-fn copy_data(test_dir: &Path, temp: &temp_dir::TempDir) -> anyhow::Result<()> {
-    let dir = match std::fs::read_dir(&test_dir.join("data")) {
-        Ok(e) => e,
-        Err(e) if e.kind() == std::io::ErrorKind::NotFound => return Ok(()),
-        Err(e) => return Err(e.into()),
-    };
-    for item in dir {
-        let item = item?;
-        if item.file_type()?.is_file() {
-            std::fs::copy(item.path(), temp.path().join(item.file_name()))?;
-        }
-    }
-    Ok(())
-}
-
-fn get_args(test_dir: &Path) -> anyhow::Result<Vec<String>> {
-    match std::fs::read_to_string(test_dir.join("args")) {
-        Ok(s) => Ok(s.lines().map(|s| s.trim().to_owned()).collect()),
-        Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(Vec::new()),
-        Err(e) => Err(e.into()),
-    }
 }
 
 /// Copies the test dir's manifest file into the temporary directory
@@ -166,7 +153,7 @@ fn make_http_request(spin: &mut Spin) -> Result<String, anyhow::Error> {
     if let Some(status) = spin.try_wait()? {
         anyhow::bail!("Spin exited early with status code {:?}", status.code());
     }
-    log::debug!("Connecting to HTTP server");
+    log::debug!("Connecting to HTTP server on port {}...", spin.port());
     let mut connection = std::net::TcpStream::connect(format!("127.0.0.1:{}", spin.port()))
         .context("could not connect to Spin web server")?;
     connection.write_all(b"GET / HTTP/1.1\r\nHost: 127.0.0.1:3000\r\n\r\n")?;
@@ -209,51 +196,55 @@ fn make_http_request(spin: &mut Spin) -> Result<String, anyhow::Error> {
 
 struct Spin {
     process: std::process::Child,
+    #[allow(dead_code)]
+    stdout: OutputStream,
+    stderr: OutputStream,
     port: u16,
 }
 
 impl Spin {
-    fn start(dir: &Path, args: &[String]) -> Result<Self, anyhow::Error> {
+    fn start(dir: &Path) -> Result<Self, anyhow::Error> {
         let port = get_random_port()?;
-        log::trace!("Starting up Spin with `spin up {}`", args.join(" "));
         let mut child = std::process::Command::new("spin")
             .arg("up")
             .current_dir(dir)
             .args(&["--listen", &format!("127.0.0.1:{port}")])
-            .args(args)
             .stdout(std::process::Stdio::piped())
             .stderr(std::process::Stdio::piped())
             .spawn()?;
-        log::debug!("Awaiting spin binary to start up");
+        let stdout = OutputStream::new(child.stdout.take().unwrap());
+        let stderr = OutputStream::new(child.stderr.take().unwrap());
+        log::debug!("Awaiting spin binary to start up on port {port}...");
+        let mut spin = Self {
+            process: child,
+            stdout,
+            stderr,
+            port,
+        };
         for _ in 0..20 {
             match std::net::TcpStream::connect(format!("127.0.0.1:{port}")) {
-                Ok(_) => {
-                    return Ok(Self {
-                        process: child,
-                        port,
-                    })
-                }
+                Ok(_) => return Ok(spin),
                 Err(e) => {
-                    log::trace!("Checking that the Spin server started returned an error: {e}")
+                    let stderr = spin.stderr.output_as_str().unwrap_or("<non-utf8>");
+                    log::trace!("Checking that the Spin server started returned an error: {e}");
+                    log::trace!("Current spin stderr = '{stderr}'");
                 }
             }
-            if let Some(status) = child.try_wait()? {
-                let mut stderr = String::new();
-                let _ = child.stderr.unwrap().read_to_string(&mut stderr);
+            if let Some(status) = spin.try_wait()? {
                 anyhow::bail!(
-                    "Spin exited early with status code {:?}\n{stderr}",
-                    status.code()
+                    "Spin exited early with status code {:?}\n{}{}",
+                    status.code(),
+                    spin.stdout.output_as_str().unwrap_or("<non-utf8>"),
+                    spin.stderr.output_as_str().unwrap_or("<non-utf8>")
                 );
             }
 
             std::thread::sleep(std::time::Duration::from_millis(250));
         }
-        child
-            .kill()
-            .context("`spin up` did not start server or error and killing the process failed")?;
-        let mut stderr = vec![0; 4048];
-        let _ = child.stderr.take().unwrap().read(&mut stderr);
-        anyhow::bail!("`spin up` did not start server or error after 5 seconds")
+        anyhow::bail!(
+            "`spin up` did not start server or error after 5 seconds. stderr:\n\t{}",
+            spin.stderr.output_as_str().unwrap_or("<non-utf8>")
+        )
     }
 
     /// The port Spin is running on
@@ -263,13 +254,6 @@ impl Spin {
 
     fn try_wait(&mut self) -> std::io::Result<Option<std::process::ExitStatus>> {
         self.process.try_wait()
-    }
-
-    fn kill(mut self) -> anyhow::Result<String> {
-        kill_process(&mut self.process);
-        let mut stderr = vec![0; 4048];
-        let _ = self.process.stderr.take().unwrap().read(&mut stderr);
-        Ok(String::from_utf8(stderr).unwrap())
     }
 }
 
@@ -297,8 +281,49 @@ fn component_path(name: &str) -> PathBuf {
         .join("component.wasm")
 }
 
+/// Uses a track to ge a random unused port
 fn get_random_port() -> anyhow::Result<u16> {
     Ok(std::net::TcpListener::bind("localhost:0")?
         .local_addr()?
         .port())
+}
+
+/// Helper for reading from a child process stream in a non-blocking way
+struct OutputStream {
+    rx: std::sync::mpsc::Receiver<Result<Vec<u8>, std::io::Error>>,
+    buffer: Vec<u8>,
+}
+
+impl OutputStream {
+    fn new<R: Read + Send + 'static>(mut stream: R) -> Self {
+        let (tx, rx) = std::sync::mpsc::channel();
+        std::thread::spawn(move || {
+            let mut buffer = vec![0; 1024];
+            loop {
+                if tx
+                    .send(stream.read(&mut buffer).map(|n| buffer[..n].to_vec()))
+                    .is_err()
+                {
+                    break;
+                }
+            }
+        });
+        Self {
+            rx,
+            buffer: Vec::new(),
+        }
+    }
+
+    /// Get the output of the stream so far
+    fn output(&mut self) -> &[u8] {
+        while let Ok(Ok(s)) = self.rx.try_recv() {
+            self.buffer.extend(s);
+        }
+        &self.buffer
+    }
+
+    /// Get the output of the stream so far
+    fn output_as_str(&mut self) -> Option<&str> {
+        std::str::from_utf8(self.output()).ok()
+    }
 }

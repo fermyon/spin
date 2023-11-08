0e35b59149dad28e33496b82bcfff1cbfe65e71e
Ryan Levick <ryan.levick@fermyon.com>
2023-11-08 07:50:12 +0700

    Remove the http crate from the Rust http template.
    While we allow the usage of the http crate, using only types defined by
    the SDK is simpler (especially considering the SDK deliberately has a
    simpler API than the HTTP crate).
    
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- templates/http-rust/content/src/lib.rs
+++ templates/http-rust/content/src/lib.rs
@@ -1,12 +1,13 @@
-use spin_sdk::http::{IntoResponse, Request};
+use spin_sdk::http::{IntoResponse, Request, Response};
 use spin_sdk::http_component;
 
 /// A simple Spin HTTP component.
 #[http_component]
 fn handle_{{project-name | snake_case}}(req: Request) -> anyhow::Result<impl IntoResponse> {
     println!("Handling request to {:?}", req.header("spin-full-url"));
-    Ok(http::Response::builder()
+    Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
-        .body("Hello, Fermyon")?)
+        .body("Hello, Fermyon")
+        .build())
 }

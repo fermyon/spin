85c335992a721ace52640490534e1fd5cc426c5c
itowlson <ivan.towlson@fermyon.com>
2023-12-07 15:40:23 +1300

    Port sqlite-libsql to new libsql client
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- crates/sqlite-libsql/src/lib.rs
+++ crates/sqlite-libsql/src/lib.rs
@@ -1,19 +1,14 @@
-mod split;
-
 use spin_world::v2::sqlite::{self, RowResult};
 
 #[derive(Clone)]
 pub struct LibsqlClient {
-    inner: libsql_client::http::Client,
+    inner: libsql::Connection,
 }
 
 impl LibsqlClient {
     pub fn create(url: &str, token: String) -> anyhow::Result<Self> {
-        let config = libsql_client::Config::new(url)?.with_auth_token(token);
-        let inner = libsql_client::http::Client::from_config(
-            libsql_client::http::InnerClient::Reqwest(libsql_client::reqwest::HttpClient::new()),
-            config,
-        )?;
+        let db = libsql::Database::open_remote(url, token)?;
+        let inner = db.connect()?;
         Ok(Self { inner })
     }
 }
@@ -25,69 +20,72 @@ impl spin_sqlite::Connection for LibsqlClient {
         query: &str,
         parameters: Vec<sqlite::Value>,
     ) -> Result<sqlite::QueryResult, sqlite::Error> {
-        let stmt =
-            libsql_client::statement::Statement::with_args(query, &convert_parameters(&parameters));
-        let client = self.inner.clone();
-
-        let result = client
-            .execute(stmt)
+        let result = self
+            .inner
+            .query(query, convert_parameters(&parameters))
             .await
             .map_err(|e| sqlite::Error::Io(e.to_string()))?;
 
         Ok(sqlite::QueryResult {
-            columns: result.columns,
-            rows: convert_rows(result.rows),
+            columns: columns(&result),
+            rows: convert_rows(result).map_err(|e| sqlite::Error::Io(e.to_string()))?,
         })
     }
 
     async fn execute_batch(&self, statements: &str) -> anyhow::Result<()> {
-        let client = libsql_client::Client::Http(self.inner.clone());
-
-        // Unfortunately, the libsql library requires that the statements are already split
-        // into individual statement strings which requires us to parse the supplied SQL string.
-        let stmts = split::split_sql(statements)
-            .map(|res| Ok(res?.to_string()))
-            .collect::<anyhow::Result<Vec<_>>>()?
-            .into_iter()
-            .map(libsql_client::Statement::from);
-
-        let _ = client.batch(stmts).await?;
+        self.inner.execute_batch(statements).await?;
 
         Ok(())
     }
 }
 
-fn convert_rows(rows: Vec<libsql_client::Row>) -> Vec<RowResult> {
-    rows.into_iter()
-        .map(|r| {
-            let values = r.values.into_iter().map(convert_value).collect();
-            RowResult { values }
-        })
+fn columns(rows: &libsql::Rows) -> Vec<String> {
+    (0..rows.column_count())
+        .map(|index| rows.column_name(index).unwrap_or("").to_owned())
         .collect()
 }
 
-fn convert_value(v: libsql_client::Value) -> sqlite::Value {
-    use libsql_client::Value;
+fn convert_rows(mut rows: libsql::Rows) -> anyhow::Result<Vec<RowResult>> {
+    let mut result_rows = vec![];
+
+    let column_count = rows.column_count();
+
+    while let Some(row) = rows.next()? {
+        result_rows.push(convert_row(row, column_count));
+    }
+
+    Ok(result_rows)
+}
+
+fn convert_row(row: libsql::Row, column_count: i32) -> RowResult {
+    let values = (0..column_count)
+        .map(|index| convert_value(row.get_value(index).unwrap()))
+        .collect();
+    RowResult { values }
+}
+
+fn convert_value(v: libsql::Value) -> sqlite::Value {
+    use libsql::Value;
 
     match v {
         Value::Null => sqlite::Value::Null,
-        Value::Integer { value } => sqlite::Value::Integer(value),
-        Value::Float { value } => sqlite::Value::Real(value),
-        Value::Text { value } => sqlite::Value::Text(value),
-        Value::Blob { value } => sqlite::Value::Blob(value),
+        Value::Integer(value) => sqlite::Value::Integer(value),
+        Value::Real(value) => sqlite::Value::Real(value),
+        Value::Text(value) => sqlite::Value::Text(value),
+        Value::Blob(value) => sqlite::Value::Blob(value),
     }
 }
 
-fn convert_parameters(parameters: &[sqlite::Value]) -> Vec<libsql_client::Value> {
-    use libsql_client::Value;
+fn convert_parameters(parameters: &[sqlite::Value]) -> Vec<libsql::Value> {
+    use libsql::Value;
 
     parameters
         .iter()
         .map(|v| match v {
-            sqlite::Value::Integer(value) => Value::Integer { value: *value },
-            sqlite::Value::Real(value) => Value::Float { value: *value },
-            sqlite::Value::Text(t) => Value::Text { value: t.clone() },
-            sqlite::Value::Blob(b) => Value::Blob { value: b.clone() },
+            sqlite::Value::Integer(value) => Value::Integer(*value),
+            sqlite::Value::Real(value) => Value::Real(*value),
+            sqlite::Value::Text(t) => Value::Text(t.clone()),
+            sqlite::Value::Blob(b) => Value::Blob(b.clone()),
             sqlite::Value::Null => Value::Null,
         })
         .collect()

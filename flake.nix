9eacf0933e61cbddcdecd62bf80a60ab15f1e093
Danielle Lancashire <dani@builds.terrible.systems>
2023-12-27 20:11:28 +0100

    chore(flake): format
    Signed-off-by: Danielle Lancashire <dani@builds.terrible.systems>

--- flake.nix
+++ flake.nix
@@ -1,11 +1,10 @@
-
 {
   description = "Nix Devshell for Spin";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
@@ -16,9 +15,9 @@
           inherit system overlays;
         };
         rustTarget = pkgs.rust-bin.stable.latest.default.override {
-              extensions= [ "rust-src" "rust-analyzer" ];
-              targets = [ "wasm32-wasi" "wasm32-unknown-unknown" ];
-            };
+          extensions = [ "rust-src" "rust-analyzer" ];
+          targets = [ "wasm32-wasi" "wasm32-unknown-unknown" ];
+        };
       in
       with pkgs;
       {
@@ -33,7 +32,7 @@
 
           shellHook = ''
             '';
-        RUST_SRC_PATH = "${rustTarget}/lib/rustlib/src/rust/library";
+          RUST_SRC_PATH = "${rustTarget}/lib/rustlib/src/rust/library";
 
         };
       }

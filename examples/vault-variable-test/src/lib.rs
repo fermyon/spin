039c561b188bec66087810404f12be23773c1aeb
Kate Goldenring <kate.goldenring@fermyon.com>
2024-02-29 15:28:40 -0800

    Use constant time comparison library in variables example
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- examples/vault-variable-test/src/lib.rs
+++ examples/vault-variable-test/src/lib.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+use constant_time_eq::constant_time_eq;
 use spin_sdk::{
     http::{IntoResponse, Request, Response},
     http_component, variables,
@@ -8,7 +9,7 @@ use spin_sdk::{
 fn handle_vault_variable_test(req: Request) -> Result<impl IntoResponse> {
     let attempt = std::str::from_utf8(req.body()).unwrap();
     let expected = variables::get("token").context("could not get variable")?;
-    let response = if constant_time_compare(&expected, attempt) {
+    let response = if constant_time_eq(&expected.into_bytes(), attempt.as_bytes()) {
         "accepted"
     } else {
         "denied"
@@ -19,24 +20,4 @@ fn handle_vault_variable_test(req: Request) -> Result<impl IntoResponse> {
         .header("content-type", "application/json")
         .body(response_json)
         .build())
-}
-
-fn constant_time_compare(a: &str, b: &str) -> bool {
-    let mut diff = 0;
-    if a.len() != b.len() {
-        return false;
-    }
-    for (a, b) in a.bytes().zip(b.bytes()) {
-        diff |= a ^ b;
-    }
-    diff == 0
-}
-
-mod test {
-    #[test]
-    fn test_constant_time_compare() {
-        assert!(super::constant_time_compare("test", "test"));
-        assert!(!super::constant_time_compare("test", "test1"));
-        assert!(!super::constant_time_compare("test", "some"));
-    }
 }

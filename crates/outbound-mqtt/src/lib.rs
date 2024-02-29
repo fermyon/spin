59e4c6f588db939c616ee4e6c8e16b33509ec38f
Suneet Nangia <suneetnangia@gmail.com>
2024-02-29 21:33:13 +0000

    Added username-password auth for MQTT publisher, fixed other minor issues
    Signed-off-by: Suneet Nangia <suneetnangia@gmail.com>

--- crates/outbound-mqtt/src/lib.rs
+++ crates/outbound-mqtt/src/lib.rs
@@ -32,13 +32,16 @@ impl OutboundMqtt {
     async fn establish_connection(
         &mut self,
         address: String,
+        username: String,
+        password: String,
         keepaliveinterval: Duration,
     ) -> Result<Result<Resource<MqttConnection>, Error>> {
         Ok(async {
             let client = Client::new(address.as_str()).map_err(|_| Error::InvalidAddress)?;
             let conn_opts = paho_mqtt::ConnectOptionsBuilder::new()
                 .keep_alive_interval(keepaliveinterval)
-                .clean_session(true)
+                .user_name(username)
+                .password(password)
                 .finalize();
 
             client.connect(conn_opts).unwrap();
@@ -59,13 +62,20 @@ impl v2::HostConnection for OutboundMqtt {
     async fn open(
         &mut self,
         address: String,
-        keepaliveinterval: u64,
+        username: String,
+        password: String,
+        keep_alive_interval: u64,
     ) -> Result<Result<Resource<MqttConnection>, Error>> {
         if !self.is_address_allowed(&address) {
             return Ok(Err(Error::InvalidAddress));
         }
-        self.establish_connection(address, Duration::from_secs(keepaliveinterval))
-            .await
+        self.establish_connection(
+            address,
+            username,
+            password,
+            Duration::from_secs(keep_alive_interval),
+        )
+        .await
     }
 
     async fn publish(
@@ -77,8 +87,6 @@ impl v2::HostConnection for OutboundMqtt {
     ) -> Result<Result<(), Error>> {
         Ok(async {
             let client = self.get_conn(connection).await.map_err(other_error)?;
-
-            // TODO: make QoS parameterised
             let message = paho_mqtt::Message::new(&topic, payload, qos as i32);
             client.publish(message).map_err(other_error)?;
             Ok(())
@@ -98,11 +106,12 @@ fn other_error(e: impl std::fmt::Display) -> Error {
 
 /// Delegate a function call to the v2::HostConnection implementation
 macro_rules! delegate {
-    ($self:ident.$name:ident($address:expr, $dur:expr, $($arg:expr),*)) => {{
+    ($self:ident.$name:ident($address:expr, $username:expr, $password:expr, $dur:expr, $($arg:expr),*)) => {{
         if !$self.is_address_allowed(&$address) {
             return Ok(Err(v1::Error::Error));
+
         }
-        let connection = match $self.establish_connection($address, $dur).await? {
+        let connection = match $self.establish_connection($address, $username, $password, $dur).await? {
             Ok(c) => c,
             Err(_) => return Ok(Err(v1::Error::Error)),
         };
@@ -117,17 +126,25 @@ impl v1::Host for OutboundMqtt {
     async fn publish(
         &mut self,
         address: String,
+        username: String,
+        password: String,
+        keep_alive_interval: u64,
         topic: String,
         payload: Vec<u8>,
-        _qos: v1::Qos,
+        qos: v1::Qos,
     ) -> Result<Result<(), v1::Error>> {
-        // TODO: map QoS from v1 to v2 or share the enum in WITs.
         delegate!(self.publish(
             address,
-            Duration::from_secs(1),
+            username,
+            password,
+            Duration::from_secs(keep_alive_interval),
             topic,
             payload,
-            Qos::AtLeastOnce
+            match qos {
+                v1::Qos::AtMostOnce => Qos::AtMostOnce,
+                v1::Qos::AtLeastOnce => Qos::AtLeastOnce,
+                v1::Qos::ExactlyOnce => Qos::ExactlyOnce,
+            }
         ))
     }
 }
--- tests/testing-framework/services/mqtt.Dockerfile
+++ tests/testing-framework/services/mqtt.Dockerfile
@@ -1,4 +1,11 @@
-# Upgrade to mosquitto 2.x later when we implement auth.
-# Mosquitto 2.x needs auth configured.
-FROM eclipse-mosquitto:1.6.12
+# Mqtt server with pre-configured user/pass authentication.
+FROM eclipse-mosquitto:2
+RUN <<EOF
+# Create predefined hashed credentials (user, password) for Mosquitto and configure it.
+echo -e "user:\$7\$101\$myOLhDLxXIIyi7Sq\$BmKGb8smWSeWrf3Rr5Ee8MefZiPMm1EiKk+RL4BngWFjPn+P0l2t56AJi+NnoGGKyPBDv/lLLUklRwT/GNPnQA==\n" > mosquitto/config/credentials.txt
+chmod 0700 mosquitto/config/credentials.txt
+# allow_anonymous is enabled with credentials to test both use cases.
+echo -e "listener 1883\nallow_anonymous true\npassword_file mosquitto/config/credentials.txt\nlog_type all\n" > mosquitto/config/mosquitto.conf
+EOF
+
 EXPOSE 1883

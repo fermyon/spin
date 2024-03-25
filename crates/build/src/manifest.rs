146f1be1025dc2e10ccf80254edcd73aa7eb863e
itowlson <ivan.towlson@fermyon.com>
2024-03-25 15:06:20 +1300

    Worked once on my machine ship it ship it now
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- crates/build/src/manifest.rs
+++ crates/build/src/manifest.rs
@@ -1,5 +1,6 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use serde::Deserialize;
+use spin_common::ui::quoted_path;
 use std::{collections::BTreeMap, path::Path};
 
 use spin_manifest::{schema::v2, ManifestVersion};
@@ -9,11 +10,12 @@ use spin_manifest::{schema::v2, ManifestVersion};
 pub async fn component_build_configs(
     manifest_file: impl AsRef<Path>,
 ) -> Result<Vec<ComponentBuildInfo>> {
-    let manifest_text = tokio::fs::read_to_string(manifest_file).await?;
-    Ok(match ManifestVersion::detect(&manifest_text)? {
+    let app_root = manifest_file.as_ref().parent().unwrap();
+    let manifest_text = tokio::fs::read_to_string(manifest_file.as_ref()).await?;
+    match ManifestVersion::detect(&manifest_text)? {
         ManifestVersion::V1 => {
             let v1: ManifestV1BuildInfo = toml::from_str(&manifest_text)?;
-            v1.components
+            Ok(v1.components)
         }
         ManifestVersion::V2 => {
             let v2: ManifestV2BuildInfo = toml::from_str(&manifest_text)?;
@@ -21,18 +23,18 @@ pub async fn component_build_configs(
                 triggers
                     .iter()
                     .flat_map(|tr| tr.component_specs())
-                    .filter_map(|spec| spec.buildinfo())
+                    .filter_map(|spec| spec.buildinfo(app_root))
             });
             v2.components
                 .into_iter()
                 .map(|(id, mut c)| {
                     c.id = id;
-                    c
+                    Ok(c)
                 })
                 .chain(inlines)
                 .collect()
         }
-    })
+    }
 }
 
 #[derive(Deserialize)]
@@ -73,13 +75,16 @@ pub enum ComponentSpec {
     Reference(String),
     /// `{ ... }`
     Inline(Box<ComponentBuildInfo>),
+    /// `"@my-component/spin-component.toml"`
+    External(std::path::PathBuf),
 }
 
 impl ComponentSpec {
-    fn buildinfo(&self) -> Option<ComponentBuildInfo> {
+    fn buildinfo(&self, app_root: &Path) -> Option<anyhow::Result<ComponentBuildInfo>> {
         match self {
             Self::Reference(_) => None, // Will be picked up from `components` section
-            Self::Inline(cbi) => Some(*cbi.clone()),
+            Self::Inline(cbi) => Some(Ok(*cbi.clone())),
+            Self::External(path) => Some(load_cbi_from(path, app_root)),
         }
     }
 }
@@ -89,12 +94,46 @@ impl TryFrom<toml::Value> for ComponentSpec {
 
     fn try_from(value: toml::Value) -> Result<Self, Self::Error> {
         match value.as_str() {
-            Some(s) => Ok(ComponentSpec::Reference(s.to_string())),
+            Some(s) => match s.strip_prefix('@') {
+                Some(path) => Ok(ComponentSpec::External(std::path::PathBuf::from(path))),
+                None => Ok(ComponentSpec::Reference(s.to_string())),
+            }
             None => Ok(ComponentSpec::Inline(Box::new(
                 ComponentBuildInfo::deserialize(value)?,
             ))),
         }
     }
+}
+
+fn load_cbi_from(path: &Path, app_root: &Path) -> anyhow::Result<ComponentBuildInfo> {
+    // moar duplication, we hates it precious
+    let abs_path = app_root.join(&path);
+    let (abs_path, containing_dir) = if abs_path.is_file() {
+        (abs_path, path.parent().unwrap().to_owned())
+    } else if abs_path.is_dir() {
+        let inferred = abs_path.join("spin-component.toml");
+        if inferred.is_file() {
+            (inferred, path.to_owned())
+        } else {
+            anyhow::bail!("{} does not contain a spin-component.toml file", quoted_path(&abs_path));
+        }
+    } else {
+        anyhow::bail!("{} does not exist", quoted_path(abs_path));
+    };
+
+    let toml_text = std::fs::read_to_string(&abs_path)?;
+    let mut component: ComponentBuildInfo = toml::from_str(&toml_text)
+        .with_context(|| format!("{} is not a valid component manifest", quoted_path(&abs_path)))?;
+
+    if let Some(build) = &mut component.build {
+        let workdir = match &build.workdir {
+            Some(w) => containing_dir.join(w).to_string_lossy().to_string(),
+            None => containing_dir.to_string_lossy().to_string(),
+        };
+        build.workdir = Some(workdir);
+    }
+
+    Ok(component)
 }
 
 #[derive(Clone, Deserialize)]

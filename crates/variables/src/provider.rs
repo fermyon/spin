c63ffda227f31de4344eae90604c0a50018b5895
Thorsten Hans <thorsten.hans@fermyon.com>
2024-05-02 09:55:34 +0200

    feat: Add Azure Key Vault Variable Provider (#2472)
    * feat: Add Azure Key Vault Variable Provider
    
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>
    
    * chore: Applied feedback and refactored Azure Key Vault variable provider impl
    
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>
    
    * chore: update cargo locks
    
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>
    
    * chore: Remove FromStr and From<String> implementations for AzureAuthorityHost
    
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>
    
    ---------
    
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

--- crates/variables/src/provider.rs
+++ crates/variables/src/provider.rs
@@ -1,2 +1,3 @@
+pub mod azure_key_vault;
 pub mod env;
 pub mod vault;
---
+++ crates/variables/src/provider/azure_key_vault.rs
@@ -0,0 +1,84 @@
+use std::sync::Arc;
+
+use anyhow::{Context, Result};
+use async_trait::async_trait;
+use azure_core::Url;
+use azure_security_keyvault::SecretClient;
+use serde::Deserialize;
+use spin_expressions::{Key, Provider};
+use tracing::{instrument, Level};
+
+#[derive(Debug)]
+pub struct AzureKeyVaultProvider {
+    client_id: String,
+    client_secret: String,
+    tenant_id: String,
+    vault_url: String,
+    authority_host: AzureAuthorityHost,
+}
+
+impl AzureKeyVaultProvider {
+    pub fn new(
+        client_id: impl Into<String>,
+        client_secret: impl Into<String>,
+        tenant_id: impl Into<String>,
+        vault_url: impl Into<String>,
+        authority_host: impl Into<AzureAuthorityHost>,
+    ) -> Self {
+        Self {
+            client_id: client_id.into(),
+            client_secret: client_secret.into(),
+            tenant_id: tenant_id.into(),
+            vault_url: vault_url.into(),
+            authority_host: authority_host.into(),
+        }
+    }
+}
+
+#[async_trait]
+impl Provider for AzureKeyVaultProvider {
+    #[instrument(name = "spin_variables.get_from_azure_key_vault", skip(self), err(level = Level::INFO), fields(otel.kind = "client"))]
+    async fn get(&self, key: &Key) -> Result<Option<String>> {
+        let http_client = azure_core::new_http_client();
+        let credential = azure_identity::ClientSecretCredential::new(
+            http_client,
+            self.authority_host.into(),
+            self.tenant_id.to_string(),
+            self.client_id.to_string(),
+            self.client_secret.to_string(),
+        );
+
+        let secret_client = SecretClient::new(&self.vault_url, Arc::new(credential))?;
+        let secret = secret_client
+            .get(key.as_str())
+            .await
+            .context("Failed to read variable from Azure Key Vault")?;
+        Ok(Some(secret.value))
+    }
+}
+
+#[derive(Debug, Copy, Clone, Deserialize)]
+pub enum AzureAuthorityHost {
+    AzurePublicCloud,
+    AzureChina,
+    AzureGermany,
+    AzureGovernment,
+}
+
+impl Default for AzureAuthorityHost {
+    fn default() -> Self {
+        Self::AzurePublicCloud
+    }
+}
+
+impl From<AzureAuthorityHost> for Url {
+    fn from(value: AzureAuthorityHost) -> Self {
+        let url = match value {
+            AzureAuthorityHost::AzureChina => "https://login.chinacloudapi.cn/",
+            AzureAuthorityHost::AzureGovernment => "https://login.microsoftonline.us/",
+            AzureAuthorityHost::AzureGermany => "https://login.microsoftonline.de/",
+            AzureAuthorityHost::AzurePublicCloud => "https://login.microsoftonline.com/",
+        };
+        Url::parse(url).unwrap()
+    }
+}

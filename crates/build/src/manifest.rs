f54cf1f28ccf27644c79f31be07f307be188c4fc
itowlson <ivan.towlson@fermyon.com>
2024-05-23 14:52:56 +1200

    Try to load manifest during `spin build`
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- crates/build/src/manifest.rs
+++ crates/build/src/manifest.rs
@@ -5,8 +5,35 @@ use std::{collections::BTreeMap, path::Path};
 use spin_manifest::{schema::v2, ManifestVersion};
 
 /// Returns a map of component IDs to [`v2::ComponentBuildConfig`]s for the
-/// given (v1 or v2) manifest path.
+/// given (v1 or v2) manifest path. If the manifest cannot be loaded, the
+/// function attempts fallback: if fallback succeeds, result is Ok but the load error
+/// is also returned via the second part of the return value tuple.
 pub async fn component_build_configs(
+    manifest_file: impl AsRef<Path>,
+) -> Result<(Vec<ComponentBuildInfo>, Option<spin_manifest::Error>)> {
+    let manifest = spin_manifest::manifest_from_file(&manifest_file);
+    match manifest {
+        Ok(manifest) => Ok((build_configs_from_manifest(manifest), None)),
+        Err(e) => fallback_load_build_configs(&manifest_file)
+            .await
+            .map(|bc| (bc, Some(e))),
+    }
+}
+
+fn build_configs_from_manifest(
+    manifest: spin_manifest::schema::v2::AppManifest,
+) -> Vec<ComponentBuildInfo> {
+    manifest
+        .components
+        .into_iter()
+        .map(|(id, c)| ComponentBuildInfo {
+            id: id.to_string(),
+            build: c.build,
+        })
+        .collect()
+}
+
+async fn fallback_load_build_configs(
     manifest_file: impl AsRef<Path>,
 ) -> Result<Vec<ComponentBuildInfo>> {
     let manifest_text = tokio::fs::read_to_string(manifest_file).await?;

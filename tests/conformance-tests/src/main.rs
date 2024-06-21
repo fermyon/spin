cadd38a7c61c5eb959b3bcad8efb5c997ef9905c
Ryan Levick <ryan.levick@fermyon.com>
2024-06-21 14:03:42 +0200

    Use new test runner
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- tests/conformance-tests/src/main.rs
+++ tests/conformance-tests/src/main.rs
@@ -5,53 +5,44 @@ fn main() {
         .nth(1)
         .expect("expected first argument to be path to spin binary")
         .into();
-    let tests_dir = conformance_tests::download_tests().unwrap();
+    conformance_tests::run_tests(move |test| run_test(test, &spin_binary)).unwrap();
+}
 
-    for test in conformance_tests::tests(&tests_dir).unwrap() {
-        println!("Running test '{}'", test.name);
-        let mut services = Vec::new();
-        for precondition in test.config.preconditions {
-            match precondition {
-                conformance_tests::config::Precondition::HttpEcho => {
-                    services.push("http-echo".into());
-                }
-                conformance_tests::config::Precondition::KeyValueStore(_) => {}
+fn run_test(test: conformance_tests::Test, spin_binary: &std::path::Path) -> anyhow::Result<()> {
+    let mut services = Vec::new();
+    for precondition in test.config.preconditions {
+        match precondition {
+            conformance_tests::config::Precondition::HttpEcho => {
+                services.push("http-echo".into());
             }
-        }
-        let env_config = SpinCli::config(
-            SpinConfig {
-                binary_path: spin_binary.clone(),
-                spin_up_args: Vec::new(),
-                app_type: testing_framework::runtimes::SpinAppType::Http,
-            },
-            test_environment::services::ServicesConfig::new(services).unwrap(),
-            move |e| {
-                let mut manifest =
-                    test_environment::manifest_template::EnvTemplate::from_file(&test.manifest)
-                        .unwrap();
-                manifest.substitute(e, |_| None).unwrap();
-                e.write_file("spin.toml", manifest.contents())?;
-                e.copy_into(&test.component, test.component.file_name().unwrap())?;
-                Ok(())
-            },
-        );
-        let mut env = test_environment::TestEnvironment::up(env_config, |_| Ok(())).unwrap();
-        for invocation in test.config.invocations {
-            let conformance_tests::config::Invocation::Http(mut invocation) = invocation;
-            invocation.request.substitute_from_env(&mut env).unwrap();
-            let spin = env.runtime_mut();
-            let actual = invocation
-                .request
-                .send(|request| spin.make_http_request(request))
-                .unwrap();
-            if let Err(e) =
-                conformance_tests::assertions::assert_response(&invocation.response, &actual)
-            {
-                eprintln!("Test '{}' failed: {e}", test.name);
-                eprintln!("stderr: {}", spin.stderr());
-                std::process::exit(1);
-            }
+            conformance_tests::config::Precondition::KeyValueStore(_) => {}
         }
     }
-    println!("All tests passed!")
+    let env_config = SpinCli::config(
+        SpinConfig {
+            binary_path: spin_binary.to_owned(),
+            spin_up_args: Vec::new(),
+            app_type: testing_framework::runtimes::SpinAppType::Http,
+        },
+        test_environment::services::ServicesConfig::new(services)?,
+        move |e| {
+            let mut manifest =
+                test_environment::manifest_template::EnvTemplate::from_file(&test.manifest)?;
+            manifest.substitute(e, |_| None)?;
+            e.write_file("spin.toml", manifest.contents())?;
+            e.copy_into(&test.component, test.component.file_name().unwrap())?;
+            Ok(())
+        },
+    );
+    let mut env = test_environment::TestEnvironment::up(env_config, |_| Ok(()))?;
+    for invocation in test.config.invocations {
+        let conformance_tests::config::Invocation::Http(mut invocation) = invocation;
+        invocation.request.substitute_from_env(&mut env)?;
+        let spin = env.runtime_mut();
+        let actual = invocation
+            .request
+            .send(|request| spin.make_http_request(request))?;
+        conformance_tests::assertions::assert_response(&invocation.response, &actual)?;
+    }
+    Ok(())
 }

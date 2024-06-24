c0065bb2a955ac6d675773233ff5905a0693eca8
Rajat Jindal <rajatjindal83@gmail.com>
2024-06-24 14:05:33 +0530

    update tls - reword commit msg
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- crates/trigger-http/src/tls.rs
+++ crates/trigger-http/src/tls.rs
@@ -1,4 +1,4 @@
-use rustls_pemfile::{certs, pkcs8_private_keys};
+use rustls_pemfile::private_key;
 use std::{
     fs, io,
     path::{Path, PathBuf},
@@ -19,28 +19,118 @@ impl TlsConfig {
     // Creates a TLS acceptor from server config.
     pub(super) fn server_config(&self) -> anyhow::Result<TlsAcceptor> {
         let certs = load_certs(&self.cert_path)?;
-        let mut keys = load_keys(&self.key_path)?;
+        let private_key = load_keys(&self.key_path)?;
 
         let cfg = rustls::ServerConfig::builder()
-            .with_safe_defaults()
             .with_no_client_auth()
-            .with_single_cert(certs, keys.remove(0))
+            .with_single_cert(certs, private_key)
             .map_err(|e| anyhow::anyhow!("{}", e))?;
 
         Ok(Arc::new(cfg).into())
     }
 }
 
-// Loads public certificate from file.
-fn load_certs(path: impl AsRef<Path>) -> io::Result<Vec<rustls::Certificate>> {
-    certs(&mut io::BufReader::new(fs::File::open(path)?))
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
-        .map(|mut certs| certs.drain(..).map(rustls::Certificate).collect())
+// load_certs parse and return the certs from the provided file
+pub fn load_certs(
+    path: impl AsRef<Path>,
+) -> io::Result<Vec<rustls_pki_types::CertificateDer<'static>>> {
+    rustls_pemfile::certs(&mut io::BufReader::new(fs::File::open(path).map_err(
+        |err| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("failed to read cert file {:?}", err),
+            )
+        },
+    )?))
+    .into_iter()
+    .collect::<io::Result<Vec<rustls_pki_types::CertificateDer<'static>>>>()
 }
 
-// Loads private key from file.
-fn load_keys(path: impl AsRef<Path>) -> io::Result<Vec<rustls::PrivateKey>> {
-    pkcs8_private_keys(&mut io::BufReader::new(fs::File::open(path)?))
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
-        .map(|mut keys| keys.drain(..).map(rustls::PrivateKey).collect())
+// load_keys parse and return the first private key from the provided file
+pub fn load_keys(path: impl AsRef<Path>) -> io::Result<rustls_pki_types::PrivateKeyDer<'static>> {
+    private_key(&mut io::BufReader::new(fs::File::open(path).map_err(
+        |err| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("failed to read private key file {:?}", err),
+            )
+        },
+    )?))
+    .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid private key"))
+    .map(|keys| keys.unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn testdatadir() -> PathBuf {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("src");
+        path.push("testdata");
+
+        path
+    }
+
+    #[test]
+    fn test_read_non_existing_cert() {
+        let mut path = testdatadir();
+        path.push("non-existing-file.pem");
+
+        let certs = load_certs(path);
+        assert!(certs.is_err());
+        assert_eq!(certs.err().unwrap().to_string(), "failed to read cert file Os { code: 2, kind: NotFound, message: \"No such file or directory\" }");
+    }
+
+    #[test]
+    fn test_read_invalid_cert() {
+        let mut path = testdatadir();
+        path.push("invalid-cert.pem");
+
+        let certs = load_certs(path);
+        assert!(certs.is_err());
+        assert_eq!(
+            certs.err().unwrap().to_string(),
+            "section end \"-----END CERTIFICATE-----\" missing"
+        );
+    }
+
+    #[test]
+    fn test_read_valid_cert() {
+        let mut path = testdatadir();
+        path.push("valid-cert.pem");
+
+        let certs = load_certs(path);
+        assert!(certs.is_ok());
+        assert_eq!(certs.unwrap().len(), 2);
+    }
+
+    #[test]
+    fn test_read_non_existing_private_key() {
+        let mut path = testdatadir();
+        path.push("non-existing-file.pem");
+
+        let keys = load_keys(path);
+        assert!(keys.is_err());
+        assert_eq!(keys.err().unwrap().to_string(), "failed to read private key file Os { code: 2, kind: NotFound, message: \"No such file or directory\" }");
+    }
+
+    #[test]
+    fn test_read_invalid_private_key() {
+        let mut path = testdatadir();
+        path.push("invalid-private-key.pem");
+
+        let keys = load_keys(path);
+        assert!(keys.is_err());
+        assert_eq!(keys.err().unwrap().to_string(), "invalid private key");
+    }
+
+    #[test]
+    fn test_read_valid_private_key() {
+        let mut path = testdatadir();
+        path.push("valid-private-key.pem");
+
+        let keys = load_keys(path);
+        assert!(keys.is_ok());
+    }
 }

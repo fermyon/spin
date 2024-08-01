5fd3054a94a21b3cc5e1c534cddf23cfbe76b713
Ryan Levick <ryan.levick@fermyon.com>
2024-08-01 15:31:13 +0200

    Run v0.1.0 of the conformance tests
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- tests/conformance-tests/src/main.rs
+++ tests/conformance-tests/src/main.rs
@@ -3,5 +3,8 @@ fn main() {
         .nth(1)
         .expect("expected first argument to be path to spin binary")
         .into();
-    conformance_tests::run_tests(move |test| conformance::run_test(test, &spin_binary)).unwrap();
+    conformance_tests::run_tests("v0.1.0", move |test| {
+        conformance::run_test(test, &spin_binary)
+    })
+    .unwrap();
 }

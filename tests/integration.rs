03ba8630990036c1669316395d0112f0adebfd32
Ryan Levick <ryan.levick@fermyon.com>
2024-08-27 17:59:06 +0200

    Ignore Swift and Grain template integration tests
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -628,6 +628,7 @@ Caused by:
     #[test]
     #[cfg(target_arch = "x86_64")]
     #[cfg(feature = "extern-dependencies-tests")]
+    #[ignore = "https://github.com/fermyon/spin/issues/2774"]
     fn http_grain_template_smoke_test() -> anyhow::Result<()> {
         http_smoke_test_template(
             "http-grain",
@@ -656,6 +657,7 @@ Caused by:
 
     #[test]
     #[cfg(feature = "extern-dependencies-tests")]
+    #[ignore = "https://github.com/fermyon/spin/issues/2774"]
     fn http_swift_template_smoke_test() -> anyhow::Result<()> {
         http_smoke_test_template(
             "http-swift",

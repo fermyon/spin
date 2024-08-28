cb31a766b9ac5a83638731bdc23a4c98c1ab0c5b
Lann Martin <lann.martin@fermyon.com>
2024-08-28 08:48:58 -0400

    Use Swift 6.0 for tests
    Signed-off-by: Lann Martin <lann.martin@fermyon.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -657,7 +657,6 @@ Caused by:
 
     #[test]
     #[cfg(feature = "extern-dependencies-tests")]
-    #[ignore = "https://github.com/fermyon/spin/issues/2774"]
     fn http_swift_template_smoke_test() -> anyhow::Result<()> {
         http_smoke_test_template(
             "http-swift",

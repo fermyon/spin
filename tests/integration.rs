70254dbaa620a24d1beafeee2cf4b032f4668875
Lann Martin <lann.martin@fermyon.com>
2024-08-30 14:12:44 -0400

    Remove swift template
    Signed-off-by: Lann Martin <lann.martin@fermyon.com>

--- tests/integration.rs
+++ tests/integration.rs
@@ -656,21 +656,6 @@ Caused by:
 
     #[test]
     #[cfg(feature = "extern-dependencies-tests")]
-    #[ignore = "https://github.com/fermyon/spin/issues/2774"]
-    fn http_swift_template_smoke_test() -> anyhow::Result<()> {
-        http_smoke_test_template(
-            "http-swift",
-            None,
-            None,
-            &[],
-            |_| Ok(()),
-            HashMap::default(),
-            "Hello from WAGI/1!\n",
-        )
-    }
-
-    #[test]
-    #[cfg(feature = "extern-dependencies-tests")]
     fn http_php_template_smoke_test() -> anyhow::Result<()> {
         super::testcases::http_smoke_test_template_with_route(
             "http-php",
